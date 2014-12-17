0154f7831408b76072d91b978370e4440aac9440
Joseph Snyder <joe.snyder@kitware.com>
2014-12-17 13:26:15 -0500

    Remove Cache Object routines from Uncategorized
    Remove the two generated Cache Object script routines from the
    Uncategorized package.

--- Packages/Uncategorized/Routines/MPIPSIM.PSIMWebServicePort.1.m
+++
@@ -1,6 +0,0 @@
- ;MPIPSIM.PSIMWebServicePort.1
- ;(C)InterSystems, generated for class MPIPSIM.PSIMWebServicePort.  Do NOT edit. 08/23/2013 09:38:05AM
- ;;51734B6F;MPIPSIM.PSIMWebServicePort
- ;
-zexecute(requestXML) public {
- Quit ..WebMethod("execute").Invoke($this,"",.requestXML) }
