8e481809c46cf073f7cef0e8f6c932a0831112f7
Joseph Snyder <joe.snyder@kitware.com>
2016-12-12 11:57:01 -0500

    Install: GMTS*2.7*117
    Patch Subject:  Remove Collaborative Stage + Other Updates For ONC Remote Data View
    Description:
    
    ============
     This patch contains enhancements that extend and improve the functionality
     of the VistA Health Summary package.  The purpose of this release is to update
     the functionality as described below.
     1. Update of the Health Summary ONC Remote Data View
     The ONC Remote Data View in Health Summary was outdated and needed several
     updates, additions and deletions.  The changes are listed below:
     a) The Collaborative Staging section was removed from the Health Summary ONC
     Remote Data View.  This was required due to the display of old/outdated AJCC
     6th Edition fields and also, since Collaborative Staging of Malignant Tumor
     TNM values will be retired with 2016 and later cases, these obsolete fields
     needed to be removed from the displays anyway.
     b) The Date of First Contact (#165.5, #155) and Laterality (#165.5, #28)
     fields were added to the display in the Cancer Identification section.
     c) The following fields were added to the First Course of Treatment section:
     Surgery of Primary Site @FAC (#165.5, #50.3)
     Surgery of Other Site (#165.5, #139.2)
     Surg of Other Site @FAC (#165.5, #139.3)
     METS Site Resected (#165.5, #139.6)
     Radiation @FAC (#165.5, #51.4)
     Chemotherapy @FAC (#165.5, #53.3)
     Hormone Therapy @FAC (#165.5, #54.3)
     Immunotherapy @FAC (#165.5, #55.3)
     HEMA Trans/Endocrine @FAC (#165.5, #153.2)
     Other Treatment @FAC (#165.5, #57.3)
     Palliative Care @FAC (#165.5, #13)
     d) The following fields were added under the Subsequent Course of Treatment
     section:
     Scope of LN Surgery (#165.51, #35)
     Surgical Proc/Other Site (#165.51, #36)
     METS Site Resected (#165.51, #40)
     e) The Date Last Contact (#165.5, #200) field was added to the end of
     the display.
     Patch Components
     ================
     Routines:
     ---------
     This release contains 2 routines:
     GMTSONE
     GMTSONE1
     Files & Fields Associated:
     File Name (#)              Field Name (#)                   New/Modified/Deleted
     -------------              ------------------               --------------------
     N/A
     Forms Associated:
     Form Name                File #                  New/Modified/Deleted
     ---------                ------                  --------------------
     N/A
     Mail Groups Associated:
     Mail Group Name                  New/Modified/Deleted
     ---------------                  --------------------
     N/A
     Options Associated:
     Option Name                                        Type    New/Modified/Deleted
     -----------                                        ----    --------------------
     N/A
     Protocols Associated:
     Protocol Name                     New/Modified/Deleted
     -------------                     --------------------
     N/A
     Security Keys Associated:
     Security Key Name
     -----------------
     N/A
     Templates Associated:
     Template Name              Type      File Name (#)     New/Modified/Deleted
     -------------              ----      -------------     --------------------
     N/A
     Test Sites
     ==========
     Hines VAMC
     Washington, DC VAMC
     West Haven, Ct HCS
     Documentation Retrieval:
     ========================
     There is no new documentation for this patch.
     Patch Installation:
     Pre/Post Installation Overview
     ==============================
     N/A
    
    Use default answers for KIDS load/install questions.
    
    Patch-Files: http://code.osehra.org/VistA.git/tree/master/Packages/Health+Summary/Patches/GMTS_2.7_117

--- Packages/Health Summary/Routines/GMTSONE1.m
+++ Packages/Health Summary/Routines/GMTSONE1.m
@@ -1,5 +1,5 @@
-GMTSONE1 ; HIN/GWB - Oncology Health Summary Comp ; 1/11/05 8:53am
- ;;2.7;Health Summary;**72**;Oct 20, 1995
+GMTSONE1 ; HIN/GWB,RTK - Oncology Health Summary Comp ; 1/27/16 8:53am
+ ;;2.7;Health Summary;**72,117**;Oct 20, 1995;Build 2
  ;                        
 DISPLAY ; Display Extracted Data
  S IEN=0 F CNT=1:1 S IEN=$O(^UTILITY("DIQ1",$J,165.5,IEN)) Q:IEN=""  D
@@ -10,11 +10,19 @@ DISPLAY ; Display Extracted Data
  . D CKP^GMTSUP Q:$D(GMTSQIT)
  . W:$D(^UTILITY("DIQ1",$J,165.5,IEN,2000)) !," Division...................: ",^UTILITY("DIQ1",$J,165.5,IEN,2000)
  . D CKP^GMTSUP Q:$D(GMTSQIT)
- . W !," Class of Case..............: ",^UTILITY("DIQ1",$J,165.5,IEN,.04)
+ . S TXT=^UTILITY("DIQ1",$J,165.5,IEN,.04) D TXT
+ . W !," Class of Case..............: ",TXT1
+ . D CKP^GMTSUP Q:$D(GMTSQIT)
+ . W:TXT2'="" !,?41,TXT2
  . D CKP^GMTSUP Q:$D(GMTSQIT)
  . W !," Date Dx....................: ",^UTILITY("DIQ1",$J,165.5,IEN,3)
  . D CKP^GMTSUP Q:$D(GMTSQIT)
- . W !," Primary Site...............: ",^UTILITY("DIQ1",$J,165.5,IEN,20)
+ . W !," Date of First Contact......: ",^UTILITY("DIQ1",$J,165.5,IEN,155)
+ . D CKP^GMTSUP Q:$D(GMTSQIT)
+ . W !," Primary Site...............: ",^UTILITY("DIQ1",$J,165.5,IEN,20.1)," ",^UTILITY("DIQ1",$J,165.5,IEN,20)
+ . D CKP^GMTSUP Q:$D(GMTSQIT)
+ . W !," Laterality.................: ",^UTILITY("DIQ1",$J,165.5,IEN,28)
+ . D CKP^GMTSUP Q:$D(GMTSQIT)
  . I ^UTILITY("DIQ1",$J,165.5,IEN,91)'="Complete" D  Q
  . . D CKP^GMTSUP Q:$D(GMTSQIT)
  . . W !," Abstract Status............: ",^UTILITY("DIQ1",$J,165.5,IEN,91)
@@ -62,23 +70,60 @@ DISPLAY ; Display Extracted Data
  . W !
  . D CKP^GMTSUP Q:$D(GMTSQIT)
  . I ^UTILITY("DIQ1",$J,165.5,IEN,3,"I")>3031231 D
+ . . Q  ;remove Collaborative Staging display but leave code intact
  . . W !," #",CNT," Collaborative Staging"
  . . D CKP^GMTSUP Q:$D(GMTSQIT)
  . . W !," ------------------------"
  . . D CKP^GMTSUP Q:$D(GMTSQIT)
- . . W !," Derived AJCC T.............: ",^UTILITY("DIQ1",$J,165.5,IEN,160)
+ . . W !," Derived AJCC-6 T...........: ",^UTILITY("DIQ1",$J,165.5,IEN,160)
  . . D CKP^GMTSUP Q:$D(GMTSQIT)
- . . W !," Derived AJCC T Descriptor..: ",^UTILITY("DIQ1",$J,165.5,IEN,161)
+ . . W !," Derived AJCC-6 T Descriptor: ",^UTILITY("DIQ1",$J,165.5,IEN,161)
  . . D CKP^GMTSUP Q:$D(GMTSQIT)
- . . W !," Derived AJCC N.............: ",^UTILITY("DIQ1",$J,165.5,IEN,162)
+ . . W !," Derived AJCC-6 N...........: ",^UTILITY("DIQ1",$J,165.5,IEN,162)
  . . D CKP^GMTSUP Q:$D(GMTSQIT)
- . . W !," Derived AJCC N Descriptor..: ",^UTILITY("DIQ1",$J,165.5,IEN,163)
+ . . W !," Derived AJCC-6 N Descriptor: ",^UTILITY("DIQ1",$J,165.5,IEN,163)
  . . D CKP^GMTSUP Q:$D(GMTSQIT)
- . . W !," Derived AJCC M.............: ",^UTILITY("DIQ1",$J,165.5,IEN,164)
+ . . W !," Derived AJCC-6 M...........: ",^UTILITY("DIQ1",$J,165.5,IEN,164)
  . . D CKP^GMTSUP Q:$D(GMTSQIT)
- . . W !," Derived AJCC M Descriptor..: ",^UTILITY("DIQ1",$J,165.5,IEN,165)
+ . . W !," Derived AJCC-6 M Descriptor: ",^UTILITY("DIQ1",$J,165.5,IEN,165)
  . . D CKP^GMTSUP Q:$D(GMTSQIT)
- . . W !," Derived AJCC Stage Group...: ",^UTILITY("DIQ1",$J,165.5,IEN,166)
+ . . W !," Derived AJCC-6 Stage Group.: ",^UTILITY("DIQ1",$J,165.5,IEN,166)
+ . . D CKP^GMTSUP Q:$D(GMTSQIT)
+ . . W !," Extension (CS).............: ",^UTILITY("DIQ1",$J,165.5,IEN,166)
+ . . D CKP^GMTSUP Q:$D(GMTSQIT)
+ . . W !," Tumor Size/Ext Eval (CS)...: ",^UTILITY("DIQ1",$J,165.5,IEN,166)
+ . . D CKP^GMTSUP Q:$D(GMTSQIT)
+ . . W !," Lymph Nodes (CS)...........: ",^UTILITY("DIQ1",$J,165.5,IEN,166)
+ . . D CKP^GMTSUP Q:$D(GMTSQIT)
+ . . W !," Lymph Nodes Eval (CS)......: ",^UTILITY("DIQ1",$J,165.5,IEN,166)
+ . . D CKP^GMTSUP Q:$D(GMTSQIT)
+ . . W !," Mets at DX (CS)............: ",^UTILITY("DIQ1",$J,165.5,IEN,34.3)
+ . . D CKP^GMTSUP Q:$D(GMTSQIT)
+ . . W !," Mets at DX-Bone............: ",^UTILITY("DIQ1",$J,165.5,IEN,34.31)
+ . . D CKP^GMTSUP Q:$D(GMTSQIT)
+ . . W !," Mets at DX-Brain...........: ",^UTILITY("DIQ1",$J,165.5,IEN,34.32)
+ . . D CKP^GMTSUP Q:$D(GMTSQIT)
+ . . W !," Mets at DX-Liver...........: ",^UTILITY("DIQ1",$J,165.5,IEN,34.33)
+ . . D CKP^GMTSUP Q:$D(GMTSQIT)
+ . . W !," Mets at DX-Lung............: ",^UTILITY("DIQ1",$J,165.5,IEN,34.34)
+ . . D CKP^GMTSUP Q:$D(GMTSQIT)
+ . . W !," Mets Eval (CS).............: ",^UTILITY("DIQ1",$J,165.5,IEN,34.4)
+ . . D CKP^GMTSUP Q:$D(GMTSQIT)
+ . . W !," Derived AJCC-7 T...........: ",^UTILITY("DIQ1",$J,165.5,IEN,160.7)
+ . . D CKP^GMTSUP Q:$D(GMTSQIT)
+ . . W !," Derived AJCC-7 T Descriptor: ",^UTILITY("DIQ1",$J,165.5,IEN,161.7)
+ . . D CKP^GMTSUP Q:$D(GMTSQIT)
+ . . W !," Derived AJCC-7 N...........: ",^UTILITY("DIQ1",$J,165.5,IEN,162.7)
+ . . D CKP^GMTSUP Q:$D(GMTSQIT)
+ . . W !," Derived AJCC-7 N Descriptor: ",^UTILITY("DIQ1",$J,165.5,IEN,163.7)
+ . . D CKP^GMTSUP Q:$D(GMTSQIT)
+ . . W !," Derived AJCC-7 M...........: ",^UTILITY("DIQ1",$J,165.5,IEN,164.7)
+ . . D CKP^GMTSUP Q:$D(GMTSQIT)
+ . . W !," Derived AJCC-7 M Descriptor: ",^UTILITY("DIQ1",$J,165.5,IEN,165.7)
+ . . D CKP^GMTSUP Q:$D(GMTSQIT)
+ . . W !," Derived AJCC-7 Stage Group.: ",^UTILITY("DIQ1",$J,165.5,IEN,166.7)
+ . . D CKP^GMTSUP Q:$D(GMTSQIT)
+ . . W !," Derived SS1977.............: ",^UTILITY("DIQ1",$J,165.5,IEN,167)
  . . D CKP^GMTSUP Q:$D(GMTSQIT)
  . . W !," Derived SS2000.............: ",^UTILITY("DIQ1",$J,165.5,IEN,168)
  . . D CKP^GMTSUP Q:$D(GMTSQIT)
@@ -98,9 +143,28 @@ DISPLAY ; Display Extracted Data
  . D CKP^GMTSUP Q:$D(GMTSQIT)
  . W:TXT2'="" !,?41,TXT2
  . D CKP^GMTSUP Q:$D(GMTSQIT)
+ . S TXT=^UTILITY("DIQ1",$J,165.5,IEN,58.7) D TXT
+ . W !," Surgery of primary site@FAC: ",^UTILITY("DIQ1",$J,165.5,IEN,50.3)," ",TXT1
+ . D CKP^GMTSUP Q:$D(GMTSQIT)
+ . W:TXT2'="" !,?41,TXT2
+ . D CKP^GMTSUP Q:$D(GMTSQIT)
+ . S TXT=^UTILITY("DIQ1",$J,165.5,IEN,139.4) D TXT
+ . W !," Surg of other site.........: ",^UTILITY("DIQ1",$J,165.5,IEN,139.2)," ",TXT1
+ . D CKP^GMTSUP Q:$D(GMTSQIT)
+ . W:TXT2'="" !,?41,TXT2
+ . D CKP^GMTSUP Q:$D(GMTSQIT)
+ . S TXT=^UTILITY("DIQ1",$J,165.5,IEN,139.5) D TXT
+ . W !," Surg of other site @FAC....: ",^UTILITY("DIQ1",$J,165.5,IEN,139.3)," ",TXT1
+ . D CKP^GMTSUP Q:$D(GMTSQIT)
+ . W:TXT2'="" !,?41,TXT2
+ . D CKP^GMTSUP Q:$D(GMTSQIT)
+ . W !," METS Site Resected.........: ",^UTILITY("DIQ1",$J,165.5,IEN,139.7)," ",^UTILITY("DIQ1",$J,165.5,IEN,139.6)
+ . D CKP^GMTSUP Q:$D(GMTSQIT)
  . W !," Reason for no surgery......: ",^UTILITY("DIQ1",$J,165.5,IEN,58)
  . D CKP^GMTSUP Q:$D(GMTSQIT)
  . W !," Radiation..................: ",^UTILITY("DIQ1",$J,165.5,IEN,51)," ",^UTILITY("DIQ1",$J,165.5,IEN,51.2)
+ . D CKP^GMTSUP Q:$D(GMTSQIT)
+ . W !," Radiation @FAC.............: ",^UTILITY("DIQ1",$J,165.5,IEN,51.5)," ",^UTILITY("DIQ1",$J,165.5,IEN,51.4)
  . D CKP^GMTSUP Q:$D(GMTSQIT)
  . W !," Regional dose:cGy..........: ",^UTILITY("DIQ1",$J,165.5,IEN,442)
  . D CKP^GMTSUP Q:$D(GMTSQIT)
@@ -112,15 +176,27 @@ DISPLAY ; Display Extracted Data
  . D CKP^GMTSUP Q:$D(GMTSQIT)
  . W !," Chemotherapy...............: ",^UTILITY("DIQ1",$J,165.5,IEN,53)," ",^UTILITY("DIQ1",$J,165.5,IEN,53.2)
  . D CKP^GMTSUP Q:$D(GMTSQIT)
+ . W !," Chemotherapy @FAC..........: ",^UTILITY("DIQ1",$J,165.5,IEN,53.4)," ",^UTILITY("DIQ1",$J,165.5,IEN,53.3)
+ . D CKP^GMTSUP Q:$D(GMTSQIT)
  . W !," Hormone therapy............: ",^UTILITY("DIQ1",$J,165.5,IEN,54)," ",^UTILITY("DIQ1",$J,165.5,IEN,54.2)
+ . D CKP^GMTSUP Q:$D(GMTSQIT)
+ . W !," Hormone therapy @FAC.......: ",^UTILITY("DIQ1",$J,165.5,IEN,54.4)," ",^UTILITY("DIQ1",$J,165.5,IEN,54.3)
  . D CKP^GMTSUP Q:$D(GMTSQIT)
  . W !," Immunotherapy..............: ",^UTILITY("DIQ1",$J,165.5,IEN,55)," ",^UTILITY("DIQ1",$J,165.5,IEN,55.2)
  . D CKP^GMTSUP Q:$D(GMTSQIT)
+ . W !," Immunotherapy @FAC.........: ",^UTILITY("DIQ1",$J,165.5,IEN,55.4)," ",^UTILITY("DIQ1",$J,165.5,IEN,55.3)
+ . D CKP^GMTSUP Q:$D(GMTSQIT)
  . W !," Hema Trans/Endocrine Proc..: ",^UTILITY("DIQ1",$J,165.5,IEN,153.1)," ",^UTILITY("DIQ1",$J,165.5,IEN,153)
+ . D CKP^GMTSUP Q:$D(GMTSQIT)
+ . W !," Hema Trans/Endocrine @FAC..: ",^UTILITY("DIQ1",$J,165.5,IEN,153.3)," ",^UTILITY("DIQ1",$J,165.5,IEN,153.2)
  . D CKP^GMTSUP Q:$D(GMTSQIT)
  . W !," Other treatment............: ",^UTILITY("DIQ1",$J,165.5,IEN,57)," ",^UTILITY("DIQ1",$J,165.5,IEN,57.2)
  . D CKP^GMTSUP Q:$D(GMTSQIT)
+ . W !," Other treatment @FAC.......: ",^UTILITY("DIQ1",$J,165.5,IEN,57.4)," ",^UTILITY("DIQ1",$J,165.5,IEN,57.3)
+ . D CKP^GMTSUP Q:$D(GMTSQIT)
  . W !," Palliative Care............: ",^UTILITY("DIQ1",$J,165.5,IEN,12)
+ . D CKP^GMTSUP Q:$D(GMTSQIT)
+ . W !," Palliative Care @FAC.......: ",^UTILITY("DIQ1",$J,165.5,IEN,13)
  . D CKP^GMTSUP Q:$D(GMTSQIT)
  . W !," Protocol eligibility status: ",^UTILITY("DIQ1",$J,165.5,IEN,346)
  . D CKP^GMTSUP Q:$D(GMTSQIT)
@@ -141,6 +217,12 @@ DISPLAY ; Display Extracted Data
  . . . D CKP^GMTSUP Q:$D(GMTSQIT)
  . . . W:TXT2'="" !,?41,TXT2
  . . . D CKP^GMTSUP Q:$D(GMTSQIT)
+ . . . W !,"    Scope of LN Surgery.....: ",$G(ONC("SUB",IEN,SUBIEN,38))," ",$G(ONC("SUB",IEN,SUBIEN,35))
+ . . . D CKP^GMTSUP Q:$D(GMTSQIT)
+ . . . W !,"    Surgical Proc/Other Site: ",$G(ONC("SUB",IEN,SUBIEN,39))," ",$G(ONC("SUB",IEN,SUBIEN,36))
+ . . . D CKP^GMTSUP Q:$D(GMTSQIT)
+ . . . W !,"    METS Site Resected......: ",$G(ONC("SUB",IEN,SUBIEN,41))," ",$G(ONC("SUB",IEN,SUBIEN,40))
+ . . . D CKP^GMTSUP Q:$D(GMTSQIT)
  . . . W !,"    Radiation...............: ",$G(ONC("SUB",IEN,SUBIEN,.051))," ",$G(ONC("SUB",IEN,SUBIEN,.05))
  . . . D CKP^GMTSUP Q:$D(GMTSQIT)
  . . . W !,"    Chemotherapy............: ",$G(ONC("SUB",IEN,SUBIEN,.061))," ",$G(ONC("SUB",IEN,SUBIEN,.06))
@@ -160,6 +242,8 @@ DISPLAY ; Display Extracted Data
  . . . W !
  . . . D CKP^GMTSUP Q:$D(GMTSQIT)
  . W !," Cancer Status..............: ",^UTILITY("DIQ1",$J,165.5,IEN,95)
+ . D CKP^GMTSUP Q:$D(GMTSQIT)
+ . W !," Date Last Contact..........: ",^UTILITY("DIQ1",$J,165.5,IEN,200)
  . D CKP^GMTSUP Q:$D(GMTSQIT)
  . W !
  . D CKP^GMTSUP Q:$D(GMTSQIT)
@@ -182,8 +266,14 @@ DISPLAY ; Display Extracted Data
  . D CKP^GMTSUP Q:$D(GMTSQIT)
  . W !
  . D CKP^GMTSUP Q:$D(GMTSQIT)
- W !
- D CKP^GMTSUP Q:$D(GMTSQIT)
+ S IEN=0 F CNT=1:1 S IEN=$O(^UTILITY("DIQ1",$J,165.5,IEN)) Q:IEN=""  D
+ . D CKP^GMTSUP Q:$D(GMTSQIT)
+ . W !," Primary Site...............: ",^UTILITY("DIQ1",$J,165.5,IEN,20.1)," ",^UTILITY("DIQ1",$J,165.5,IEN,20)
+ . D CKP^GMTSUP Q:$D(GMTSQIT)
+ . W !," Cancer Status..............: ",^UTILITY("DIQ1",$J,165.5,IEN,95)
+ . D CKP^GMTSUP Q:$D(GMTSQIT)
+ . W !
+ . D CKP^GMTSUP Q:$D(GMTSQIT)
  Q
 TXT ; Print Formatted Text
  S (TXT1,TXT2)="",LOS=$L($G(TXT)) I LOS<40 S TXT1=$G(TXT) Q
--- Packages/Registration/Routines/DPTLK7.m
+++ Packages/Registration/Routines/DPTLK7.m
@@ -1,5 +1,5 @@
 DPTLK7 ;OAK/ELZ - MAS PATIENT LOOKUP ENTERPRISE SEARCH ; 8/24/15 2:38pm
- ;;5.3;Registration;**915,919**;Aug 13, 1993;Build 4
+ ;;5.3;Registration;**915**;Aug 13, 1993;Build 6
  ;
 SEARCH(DGX,DGXOLD) ; do a search, pass in what the user entered
  ; DGX is what the user originally entered, name is assumed unless it
@@ -36,7 +36,10 @@ PROMPT I DGX?9N S DGFLDS(.09)=DGX,DGX=""
  ; call MPI to get data
  W !!,"Searching the MVI..."
  D FORMAT(.DGMPI,.DG20NAME,.DGFLDS)
- D PATIENT^MPIFXMLP(.DGMPIR,.DGMPI)
+ ; OSEHRA Change by J. Snyder <http://issues.osehra.org/browse/OAT-189>
+ ; Commented out the following line(s)
+ ; D PATIENT^MPIFXMLP(.DGMPIR,.DGMPI)
+ ; end OSEHRA Change
  S DGMCID=$G(DGMPIR("mcid"))
  ;
  ; too many matches found, they need to get the numbers down, re-prompt
@@ -55,7 +58,10 @@ PROMPT I DGX?9N S DGFLDS(.09)=DGX,DGX=""
  . S DGMPIR(+$O(DGMPIR(0)),"DFN")=DGDFN
  . ;
  . S DGMPIR("mcid")=DGMCID
- . D MPIADD(.DGMPIR)
+ . ; OSEHRA Change by J. Snyder <http://issues.osehra.org/browse/OAT-189>
+ . ; Commented out the following line(s)
+ . ; D MPIADD(.DGMPIR)
+ . ; end OSEHRA Change
  ;
  ; do I have some records that are in autolink threshold? - key required
  S X=0 F  S X=$O(DGMPIR(X)) Q:'X  I $G(DGMPIR(X,"Score"))'<$G(DGMPIR("matchThreshold")) S DGKEYREQ=1
@@ -73,7 +79,10 @@ PROMPT I DGX?9N S DGFLDS(.09)=DGX,DGX=""
  . S DGMPIR(+$O(DGMPIR(0)),"DFN")=DGDFN
  . ;
  . S DGMPIR("mcid")=DGMCID
- . D MPIADD(.DGMPIR)
+ . ; OSEHRA Change by J. Snyder <http://issues.osehra.org/browse/OAT-189>
+ . ; Commented out the following line(s)
+ . ; D MPIADD(.DGMPIR)
+ . ; end OSEHRA Change
  . W !
  . ;
  . ; if known to ESR, send Z11 and monitor for return data
@@ -108,7 +117,10 @@ PROMPT I DGX?9N S DGFLDS(.09)=DGX,DGX=""
  . S DGMPIR(+$O(DGMPIR(0)),"DFN")=DGDFN
  . S DGMPIR("AddType")=$S(DGKEYREQ:"Explicit",1:"Implicit")
  . S DGMPIR("mcid")=DGMCID
- . D MPIADD(.DGMPIR)
+ . ; OSEHRA Change by J. Snyder <http://issues.osehra.org/browse/OAT-189>
+ . ; Commented out the following line(s)
+ . ;D MPIADD(.DGMPIR)
+ . ; end OSEHRA Change
  ;
 QUIT Q $S(DGDFN:DGDFN,1:0)
  ;
@@ -342,7 +354,7 @@ ADD(DGF,DG20NAME) ; - stuff in patient
  ;
  S DIC("DR")="",REQ="^.02^.03^.09^"
  S DGF=.01 F  S DGF=$O(DGF(DGF)) Q:'DGF  D
- . ; if the data has a second piece, then that's internal value to use
+ . ; if the data has a second piece, then that's interal value to use
  . S DATA=$S($P(DGF(DGF),"^",2):$P(DGF(DGF),"^",2),1:DGF(DGF))
  . I DATA]""!(REQ[("^"_DGF_"^")) S DIC("DR")=DIC("DR")_DGF_$S(DATA]"":"////"_DATA,1:"")_";"
  ; patient type
@@ -353,8 +365,6 @@ ADD(DGF,DG20NAME) ; - stuff in patient
  S DIC("DR")=DIC("DR")_".301///N;"
  ; date added
  S DIC("DR")=DIC("DR")_".097////"_DT
- ; who added
- S:$G(DUZ) DIC("DR")=DIC("DR")_";.096////"_DUZ
  ;
  S X=DGF(.01),DIC="^DPT(",DIC(0)="L",DLAYGO=2,VAFCNO=1
  D FILE^DICN
@@ -369,9 +379,6 @@ ADD(DGF,DG20NAME) ; - stuff in patient
  I $D(FDA) D
  . N DG20NAME
  . D UPDATE^DIE("","FDA")
- ;
- ; send bulletin new patient added to system
- I SAVY>0 D BULL(SAVY)
  ;
  Q SAVY
  ;
@@ -395,16 +402,3 @@ PSEUDO(NAM,DOB) ; - return pseudo ssn
  S L2=Z,Z=L3 D CON^DGRPDD1 S L3=Z
  Q L2_L1_L3_$E(DOB,4,7)_$E(DOB,2,3)_"P"
  ;
-BULL(SAVY) ; - send bulletin that new patient added
- N DGTEXT,DGNAM,DGSSN,DGDOB,DGB,DGZ
- S DGB=2
- S DGZ=$G(^DPT(SAVY,0))
- S DGNAM=$P(DGZ,"^"),DGSSN=$P(DGZ,"^",9),DGDOB=$P(DGZ,"^",3)
- S DGSSN=$E(DGSSN,1,3)_"-"_$E(DGSSN,4,5)_"-"_$E(DGSSN,6,10)
- S DGDOB=$$FMTE^XLFDT(DGDOB)
- S XMSUB="NEW PATIENT ADDED TO SYSTEM"
- S DGTEXT(1,0)="NAME:  "_DGNAM
- S DGTEXT(2,0)="SSN :  "_DGSSN
- S DGTEXT(3,0)="DOB :  "_DGDOB
- D ^DGBUL
- Q
