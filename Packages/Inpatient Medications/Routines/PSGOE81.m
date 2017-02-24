4fe532164f046f6395dd65656b594e732fedfdf2
Joseph Snyder <joe.snyder@kitware.com>
2017-02-24 17:16:23 -0500

    Install: PSJ*5.0*334
    Patch Subject:  TRANSDERMAL EMERGENCY PATCH
    Description:
    
    ============
     Patch PSJ*5.0*334 will correct the exception handling for Duration of
     Administration to enforce integer only values. Patch PSJ*5.0*315
     introduced code into three routines that prompt for Duration of
     Administration.  The code originally blocked the use of non-integer
     entries, but other changes to code caused this to stop working, so that
     Decimal values can be entered (such as 23.5).  This will not compute
     correctly.  Patch PSJ*5.0*334 corrects this by making changes to allow the
     code to enforce the integer only requirement.
     *NOTE*: This patch is a follow-up for patch PSJ*5.0*315 to fix the noted
     issue.  However, there is an issue for BCMA which is part of this
     enhancement that needs to be noted.
          For non-standard schedules such as DAY@HHMM, HHMM or other odd
     schedules, Removal Times cannot be calculated due to the lack of a
     Frequency. Regardless of the Prompt for Removal in BCMA code value for
     the orderable item, no Removal Times will display in BCMA for these
     non-standard schedules. These types of schedules must be avoided for
     Medications Requiring Removal (MRR) orders.
     Patch Components:
     -----------------
     Files & Fields Associated:
     File Name (Number)      Field Name (Number)     New/Modified/Deleted
     ------------------      -------------------     --------------------
     N/A
     Forms Associated:
     Form Name       File #  New/Modified/Deleted
     ---------       ------  --------------------
     N/A
     Mail Groups Associated:
     Mail Group Name New/Modified/Deleted
     --------------- --------------------
     N/A
     Options Associated:
     Option Name     Type    New/Modified/Deleted
     -----------     ----    --------------------
     N/A
     Protocols Associated:
     Protocol Name   New/Modified/Deleted
     -------------   --------------------
     N/A
     Security Keys Associated:
     Security Key Name
     -----------------
     N/A
     Templates Associated:
     Template Name   Type    File Name (Number)  New/Modified/Deleted
     -------------   ----    ------------------  --------------------
     N/A
     Additional Information:
     New Service Requests (NSRs):
     ----------------------------
     N/A
     Patient Safety Issues (PSIs):
     -----------------------------
     N/A
     Defect Tracking System Ticket(s) & Overview:
     --------------------------------------------
     R10757134FY16: Entry of 23.5 at Duration of Administration Prompt
     produces bad calculation for time.
     Problem:
     -------
     Problem occurred when a site decided to enter 23.5 hours in at the
     Duration of Administration prompt.  This resulted in an incorrect removal
     time. Only Integer values are permitted.
     Resolution:
     ----------
     Routines PSGOE41, PSGOE81 and PSGOE91 had a change to conditional logic
     that allows pattern match code to enforce the integer requirement.
     Previously this code had become isolated and was not being executed when
     needed.
     Test Sites:
     ----------
     PITTSBURGH HCS
     VISN2/UPSTATE NY
     ST. CLOUD, MN -VAMC
     Software and Documentation Retrieval Instructions:
     ----------------------------------------------------
     N/A
     Patch Installation:
     Pre/Post Installation Overview:
     -------------------------------
     This patch only installs three routines, There are no install routines
     to delete.
    
    Use default answers for KIDS load/install questions.
    
    Patch-Files: http://code.osehra.org/VistA.git/tree/master/Packages/Inpatient+Medications/Patches/PSJ_5.0_334

--- Packages/Inpatient Medications/Routines/PSGOE81.m
+++ Packages/Inpatient Medications/Routines/PSGOE81.m
@@ -1,5 +1,5 @@
 PSGOE81 ;BIR/CML3-NON-VERIFIED ORDER EDIT (CONT.) ; 7/20/12 12:43am
- ;;5.0;INPATIENT MEDICATIONS;**26,50,64,58,82,110,111,136,113,267,315**;16 DEC 97;Build 73
+ ;;5.0;INPATIENT MEDICATIONS;**26,50,64,58,82,110,111,136,113,267,315,334**;16 DEC 97;Build 3
  ;;Per VHA Directive 2004-038, this routine should not be modified.
  ; Reference to ^PS(50.7 is supported by DBIA# 2180
  ; Reference to ^PS(51.1 is supported by DBIA 2177.
@@ -149,7 +149,8 @@ PSGDUR ; Prompt for Removal times if admin times are on 24hr rotations and Site 
  I RP="" S:$G(PSGDUR)>0 RP=($G(PSGDUR)/60)
  I RP="",$G(PSGS0XT)="D",$L(PSGSCH,"@")=2,$P(PSGSCH,"@",2) S (PSGAT,PSGRMVT)=$P(PSGSCH,"@",2) G 8
  I RP="@",PSGRF'=3 D DEL G:%'=1 PSGDUR S (PSGS0Y,PSGDUR,PSGRMVT)="",PSGRMV=-1 S:+$G(^PS(53.1,+$G(PSGORD),2.1)) (PSGDUR,PSGRMV,PSGRMVT)="@" Q
- S:(RP'="")&(RP'="@")&(+(RP)<1)&(($E(RP,1))'="?")&(RP'?1N.2N) RP="?" I RP?1."?" D DURHLP^PSGOEM(RP,PSGRF) G PSGDUR
+ I (RP'=""),(RP'="@"),($E(RP)'="^"),($E(RP)'="?") S:(RP'?1N.2N)!(+(RP)<1) RP="?"
+ I RP?1."?" D DURHLP^PSGOEM(RP,PSGRF) G PSGDUR
  I $E(RP)="^" D FF G:Y>0 @Y G PSGDUR
  I (+RP>0),'PSGIDF D  ; exclude BID,TID or QID schedules
  . S PSGDUR=(RP*60),PSGRMV=$G(PSGS0XT)-PSGDUR
