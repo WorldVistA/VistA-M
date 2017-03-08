42c9b3db8a4308e204d7fa3a9dd5358d889fdb5d
Joseph Snyder <joe.snyder@kitware.com>
2017-03-08 01:12:01 -0500

    Install: ONC*2.2*8
    Patch Subject:  Oncology RQRS Data Pull Issue
    Description:
    
    ============
     The Commission on Cancer's Rapid Quality Response System's (RQRS) utility
     under the Create RQRS Extract [ONCO RQRS EXTRACT] option is not functioning
     properly and is jeopardizing the Commission on Cancer's (CoC) compliance
     standards.  This patch corrects the issue and needs to be installed within
     30 days to meet CoC Standards timeframe.
     Solution:
     ---------
     Allow a BLANK Clinical and Pathologic TNM data fields for primaries with
     a DATE DX (#165.5, #3) of 2016 and later in option Create RQRS Extract
     [ONCO RQRS EXTRACT].  The TNM data fields associated with these changes
     are:
             CLINICAL T (#165.5, #37.1)
             CLINICAL N (#165.5, #37.2)
             CLINICAL M (#165.5, #37.3)
             PATHOLOGIC T (#165.5, #85)
             PATHOLOGIC N (#165.5, #86)
             PATHOLOGIC M (#165.5, #87)
     Test Sites:
     -----------
     Hines VAMC
     Washington, DC VAMC
     West Haven, Ct
     PATCH COMPONENTS:
     =================
     Routines:
     ---------
     ONCACD0
     ONCACD1
     ONCODIS
     Files & Fields Associated:
     --------------------------
     N/A
     Forms Associated:
     -----------------
     N/A
     Mail Groups Associated:
     -----------------------
     N/A
     Security Keys Associated:
     -------------------------
     N/A
     Templates Associated:
     ---------------------
     N/A
     Documentation Retrieval:
     ========================
     There are no documentation changes with this patch but the existing
     Oncology documentation is available on the ANONYMOUS.SOFTWARE directory
     at one of the following Office of Information (OI) Field Offices:
        Albany
        Hines
        Salt Lake City
     Sites may retrieve Oncology documentation in one of the following ways:
       (1) The preferred method is to SFTP the files from
           download.vista.domain.ext, which will transmit the files from
           the first available FTP server.
       (2) Sites may also elect to retrieve documentation directly from a
           specific server listed below:
             Albany         ftp.domain.ext
             Hines          ftp.domain.ext
             Salt Lake City ftp.domain.ext
     (3) Documentation can also be retrieved from the VistA Documentation
         Library (VDL) on the Internet at the following address,
         http://www.domain.ext/vdl.
     Patch Installation:
     ===================
     Pre/Post Installation Overview:
     -------------------------------
     There is no pre- or post-installation routine for this patch.
    
    Use default answers for KIDS load/install questions.
    
    Patch-Files: http://code.osehra.org/VistA.git/tree/master/Packages/Oncology/Patches/ONC_2.2_8

--- Packages/Oncology/Routines/ONCACD1.m
+++ Packages/Oncology/Routines/ONCACD1.m
@@ -1,7 +1,8 @@
 ONCACD1 ;Hines OIFO/GWB - Extract NAACCR/STATE/VACCR data ;09/06/11
- ;;2.2;ONCOLOGY;**1,4,5**;Jul 31, 2013;Build 6
+ ;;2.2;ONCOLOGY;**1,4,5,8**;Jul 31, 2013;Build 3
  ;
  ;P5 added in RQRS the Analytic Cases selection.
+ ;P8 allows BLANK in TNM Clin/Path data fields & others.
 EN1 ;Entry point
  K ^TMP($J)
  N EXPORT,PAGE,STOPDT,OIEN,ZTREQ
@@ -53,12 +54,12 @@ SETUP ;Loop through appropriate cross-reference
  .Q:$G(^ONCO(165.5,IEN,0))=""
  .S TPG=$P($G(^ONCO(165.5,IEN,2)),U,1)
  .S NC=0
- .F FDNUM=.03,.04,.05,.06,3,9,20,22.3,37.1,37.2,37.3,38,58.6,58.7,85,86,88 I $$GET1^DIQ(165.5,IEN,FDNUM,"I")="" S NC=1
- .I $$GET1^DIQ(165.5,IEN,3,"I")<3100000,$$GET1^DIQ(165.5,IEN,87,"I")="" S NC=1
+ .F FDNUM=.03,.05,.06,3,20,22.3 I $$GET1^DIQ(165.5,IEN,FDNUM,"I")="" S NC=1
  .Q:NC=1
  .S ONCCLCA=$E($$GET1^DIQ(165.5,IEN,.04),1,2)
  .I ($G(ONCR12)=2),((ONCCLCA=0)!(ONCCLCA>0)&(ONCCLCA<23)) D LOOP Q
  .I ($G(ONCR12)=1),(($E(TPG,3,4)=50)!($E(TPG,3,4)=18)!($E(TPG,3,4)=20))&(TPG'=67181) D LOOP  ; screen out 67181 (appendix) cases - p2.2*4
+ ;
  ;Loop through DATE CASE LAST CHANGED (165.5,198) "AAE" cross-reference
  ;Quit if "ADX" is before 2008 - p2.2*4
  I STEXT=3,($G(ONCLDT)=2) S SDT=SDT-1 F  S SDT=$O(^ONCO(165.5,"AAE",SDT)) Q:(SDT<1)!(SDT>EDT)!(OUT=1)  F  S IEN=$O(^ONCO(165.5,"AAE",SDT,IEN)) Q:IEN<1  I $$DIV^ONCFUNC(IEN)=DUZ(2) D  Q:OUT
@@ -66,20 +67,19 @@ SETUP ;Loop through appropriate cross-reference
  .Q:$P($G(^ONCO(165.5,IEN,0)),U,16)<3080101
  .S TPG=$P($G(^ONCO(165.5,IEN,2)),U,1)
  .S NC=0
- .F FDNUM=.03,.04,.05,.06,3,9,20,22.3,37.1,37.2,37.3,38,58.6,58.7,85,86,88 I $$GET1^DIQ(165.5,IEN,FDNUM,"I")="" S NC=1
- .I $$GET1^DIQ(165.5,IEN,3,"I")<3100000,$$GET1^DIQ(165.5,IEN,87,"I")="" S NC=1
+ .F FDNUM=.03,.05,.06,3,20,22.3 I $$GET1^DIQ(165.5,IEN,FDNUM,"I")="" S NC=1
  .Q:NC=1
  .S ONCCLCA=$E($$GET1^DIQ(165.5,IEN,.04),1,2)
  .I ($G(ONCR12)=2),((ONCCLCA=0)!(ONCCLCA>0)&(ONCCLCA<23)) D LOOP Q
  .I ($G(ONCR12)=1),($E(TPG,3,4)=50)!($E(TPG,3,4)=18)!($E(TPG,3,4)=20)&(TPG'=67181) D LOOP
+ ;
  ;Loop through ACCESSION NUMBER (165.5,.05) "AA" cross-reference
  I STEXT=3,($G(ONCLDT)=3) S SDT=SDT-1 F  S SDT=$O(^ONCO(165.5,"AA",SDT)) Q:(SDT<1)!(SDT>EDT)!(OUT=1)  F  S IEN=$O(^ONCO(165.5,"AA",SDT,IEN)) Q:IEN<1  I $$DIV^ONCFUNC(IEN)=DUZ(2) D  Q:OUT
  .Q:$G(^ONCO(165.5,IEN,0))=""
  .Q:$P($G(^ONCO(165.5,IEN,0)),U,16)<3080101
  .S TPG=$P($G(^ONCO(165.5,IEN,2)),U,1)
  .S NC=0
- .F FDNUM=.03,.04,.05,.06,3,9,20,22.3,37.1,37.2,37.3,38,58.6,58.7,85,86,88 I $$GET1^DIQ(165.5,IEN,FDNUM,"I")="" S NC=1
- .I $$GET1^DIQ(165.5,IEN,3,"I")<3100000,$$GET1^DIQ(165.5,IEN,87,"I")="" S NC=1
+ .F FDNUM=.03,.05,.06,3,20,22.3 I $$GET1^DIQ(165.5,IEN,FDNUM,"I")="" S NC=1
  .Q:NC=1
  .S ONCCLCA=$E($$GET1^DIQ(165.5,IEN,.04),1,2)
  .I ($G(ONCR12)=2),((ONCCLCA=0)!(ONCCLCA>0)&(ONCCLCA<23)) D LOOP Q
--- Packages/Oncology/Routines/ONCODIS.m
+++ Packages/Oncology/Routines/ONCODIS.m
@@ -1,5 +1,5 @@
 ONCODIS ;Hines OIFO/GWB - OncoTrax Banner ;09/13/11
- ;;2.2;ONCOLOGY;**1,4,7,5**;Jul 31, 2013;Build 6
+ ;;2.2;ONCOLOGY;**1,4,7,5,8**;Jul 31, 2013;Build 3
  ;
 MAIN ;OncoTrax Banner
  S RC=$$CHKVER^ONCSAPIV()
@@ -10,7 +10,7 @@ MAIN ;OncoTrax Banner
 LOGO ;Display banner
  W @IOF W !!!!!! F I=1:1:9 W !,?22,$P($T(DISP+I),";",3)
  W !!,?22,"Department of Veterans Affairs"
- W !?22,"OncoTraX V2.2 P5" Q
+ W !?22,"OncoTraX V2.2 P8" Q
 DISP ;
  ;;VVVV            VVAA
  ;; VVVV          VVAAAA
