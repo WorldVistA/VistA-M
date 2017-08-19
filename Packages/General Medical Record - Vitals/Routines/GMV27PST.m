04f1d8a6aaec4ee6c3f29dee0b3047d564ae2e6e
Joseph Snyder <joe.snyder@kitware.com>
2017-08-19 01:22:27 -0400

    Install: GMRV*5.0*27
    Patch Subject:  VITALS/VITALS MANAGER GUI 2FA IMPLEMENTATION
    Description:
    
    ============
     This patch will update the log in procedure to the Vitals User and Vitals
     Manager Graphical User Interface (GUI) to use Two Factor Authentication
     (2FA) which will require the use of a Personal Identity Verification (PIV)
     card.
     In order to make the transition to 2FA, it was also necessary to
     upgrade the Vitals User and Vitals Manager GUI to Delphi version XE8 in
     order to implement the new Remote Procedure Call (RPC) Broker allowing
     compliance.
     Note: Patch GMRV*5.0*27 includes a new GUI within the .MSI executable.
     ASSOCIATED TICKET:
     ==================
     N/A
     Associated NSR(s):
     =================
     N/A
     PARTICIPATING TEST SITES:
     =========================
     VA MARYLAND HCS
     VA Hudson Valley Health Care System, NY
     TICKET OVERVIEW:
     ================
     Problem:
     --------
     The Department of Veterans Affairs Cybersecurity Task Force has been
     tasked by the Chief Information Officer (CIO) and the Inspector General
     (IG) to address identified material weaknesses. This task force has
     mandated that all internal, user facing VA applications become two factor
     authentication (2FA) compliant by April 30, 2017.
     In order to be compliant, applications are required to authenticate users
     via Personal Identity Verification (PIV) cards.
     Note: Usernames and password will no longer be permitted for employees and
           contractors to access VA applications.
     Resolution:
     -----------
     As part of the transition to 2FA, Vitals User and Vitals Manager were
     upgraded to Delphi XE8 to allow access to the latest version of the RPC
     Broker implementing 2FA.
     Software and Documentation Retrieval Instructions:
     ==================================================
     Software being released as a host file and/or documentation describing
     the new functionality introduced by this patch are available.
     The preferred method is to retrieve files from download.vista.domain.ext.
     This transmits the files from the first available server. Sites may
     also elect to retrieve files directly from a specific server.
     Sites may retrieve the software and/or documentation directly using
     Secure File Transfer Protocol (SFTP) from the ANONYMOUS.SOFTWARE
     directory at the following OI Field Offices:
         CIO FIELD OFFICE      FTP ADDRESS                  DIRECTORY
         ----------------      -----------                  ---------
         Albany                ftp.domain.ext    []
         Hines                 ftp.domain.ext     []
         Salt Lake City        ftp.domain.ext       []
         The ZIP file contains:
         FILE NAME                 DESCRIPTION                RETRIEVAL FORMAT
         ----------                -----------                ----------------
         VITL5_P27.ZIP             Zipped file                   Binary
         CONTENTS                  DESCRIPTION                RETRIEVAL FORMAT
         --------                  -----------                ----------------
         VITL5_P27.MSI             InstallShield executable      Binary
         VITL5_P27_SRC.ZIP         Source Code, Zipped           Binary
     The User Manual (all pages) can also be retrieved from the VistA
     Documentation Library (VDL) on the VA Intranet at the following address:
             http://www.domain.ext/vdl
        File Name                 Description                 Protocol
        ---------                     -----------                 --------
        vitl5_um.pdf              User Manual                 Binary
    
    Use default answers for KIDS load/install questions.
    
    Patch-Files: http://code.osehra.org/VistA.git/tree/master/Packages/General+Medical+Record+-+Vitals/Patches/GMRV_5.0_27

---
+++ Packages/General Medical Record - Vitals/Routines/GMV27PST.m
@@ -0,0 +1,33 @@
+GMV27PST ;HIOFO/FT - FIX PARAMETER SETTINGS ;11/05/13 9:00am
+ ;;5.0;GEN. MED. REC. - VITALS;**27**;Oct 31, 2002;Build 3
+ Q
+ ; This routine uses the following IAs:
+ ; 2263 - ^XPAR                  (supported)
+ ; 
+EN ; main entry point 
+ D XPAR
+ Q
+XPAR ; Update the GUI version parameters. This subroutine is called during the
+ ; KIDS installation process.
+ ;
+ ; Variables:
+ ;  GMV:    [Private] Scratch
+ ;  GMVGUI: [Private] Current version of GUI being installed
+ ;  GMVLST: [Private] Scratch List
+ ;
+ ; NEW private variables
+ N GMV,GMVGUI,GMVLST
+ ; Announce my intentions
+ D BMES^XPDUTL("Updating system parameters.")
+ ; Set current client version
+ S GMVGUI="5.0.27.5"
+ S GMVMGUI="5.0.27.3"
+ ; Deactivate all previous versions from XPAR
+ D GETLST^XPAR(.GMVLST,"SYS","GMV GUI VERSION")
+ F GMV=0:0 S GMV=$O(GMVLST(GMV)) Q:'GMV  D
+ . D EN^XPAR("SYS","GMV GUI VERSION",$P(GMVLST(GMV),"^",1),0)
+ .Q
+ ; Add and/or activate current client versions
+ D EN^XPAR("SYS","GMV GUI VERSION","VITALS.EXE:"_GMVGUI,1)
+ D EN^XPAR("SYS","GMV GUI VERSION","VITALSMANAGER.EXE:"_GMVMGUI,1)
+ Q
