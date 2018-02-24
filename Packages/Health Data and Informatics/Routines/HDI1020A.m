0c016ede2dd0afd585016159c3a709d04c4f72d0
Joseph Snyder <joe.snyder@kitware.com>
2018-02-23 20:44:23 -0500

    Install: HDI*1.0*20
    Patch Subject:  PAYERS NATIVE DOMAIN STANDARDIZATION
    Description:
    
    ============
     ******************************* ATTENTION ********************************
     The Collaborative Terminology Tooling & Data Management (CTT DM) Native
     Domain Standardization (NDS) Payers Domain enhancement includes 3 patches
     that must be installed in the following order:
           1.  XU*8.0*683
           2.  HDI*1.0*20
           3.  IB*2.0*585
     **************************************************************************
     The post-install routine in this patch will add PAYERS Domain data
     to the HDI DOMAIN file (#7115.1). It will also add data for the
     file/fields being standardized for PAYERS to the HDIS FILE/FIELD
     file (#7115.6). These entries are being added to the files for VHA Unique
     Identifier (VUID) Seeding which is part of standardizing the MASTER TYPE
     OF PLAN (#355.99).
     Remedy Tickets addressed by this patch:
     ======================================
     NA
     Test Sites:
     ----------
     Altoona, PA VAMC
     Manchester, NH VAMC
     Documentation Retrieval Instructions:
     ------------------------------------
     Software and Documentation Retrieval Instructions:
     ----------------------------------------------------
     The software is being released as a FORUM patch. There is no new
     documentation being released with this patch. For additional information,
     see the FORUM patch description for patch IB*2.0*585.
    
    Use default answers for KIDS load/install questions.
    
    Patch-Files: http://code.osehra.org/VistA.git/tree/master/Packages/Health+Data+and+Informatics/Patches/HDI_1.0_20

---
+++ Packages/Health Data and Informatics/Routines/HDI1020A.m
@@ -0,0 +1,54 @@
+HDI1020A ;DAL/JCH - PATCH 20 POST INSTALL;06/15/2017
+ ;;1.0;HEALTH DATA & INFORMATICS;**20**;Feb 22, 2005;Build 3
+ ;
+POST ;
+ N DOMAIN,HDIDOM,HDIERROR,HDIMSG
+ ;
+ S HDIMSG(1)="Post-Installation (POST^HDI1020A) will now be run."
+ S HDIMSG(2)=" "
+ D MES^XPDUTL(.HDIMSG) K HDIMSG
+ ;
+ S DOMAIN="PAYERS" ; domain to be added to HDIS DOMAIN File #7115.1
+ ;
+ ; add domain
+ I '+$$UPDTDOM^HDISVCUT(DOMAIN) D  Q
+ . D MES^XPDUTL("***** Error adding the "_DOMAIN_" domain to the HDIS DOMAIN FILE #7115.1."),PSTHALT("")
+ ;
+ ; get domain IEN
+ I '+$$GETIEN^HDISVF09(DOMAIN,.HDIDOM) D  Q
+ . D MES^XPDUTL("***** Error retrieving the IEN for the "_DOMAIN_" domain."),PSTHALT("")
+ ;
+ ; verify domain IEN
+ I '+HDIDOM D  Q
+ . D MES^XPDUTL("***** Error verifying the IEN for the "_DOMAIN_" domain."),PSTHALT("")
+ ;
+ ; get files & fields to be added to File #7115.6
+ N DATA,LINE F LINE=1:1 S DATA=$P($T(DATA+LINE),";;",2) Q:DATA=""  D
+ . N FILE,FIELD,HDIDATA,HDIERMSG
+ . S FILE=$P(DATA,U),FIELD=$P(DATA,U,2)
+ . I +$$GETIEN^HDISVF05(FILE,FIELD) Q  ; quit if entry already exists
+ . S HDIDATA(FILE)=FIELD
+ . ; add entry to HDIS FILE/FIELD File #7115.6
+ . I '+$$ADDDFFS^HDISVF09(HDIDOM,.HDIDATA,.HDIERMSG) D
+ . . I '$D(HDIERROR) D MES^XPDUTL("***** "_"Error updating File #7115.6")
+ . . S HDIERROR=1
+ . . D MES^XPDUTL("***** "_HDIERMSG)
+ ;
+ I +$D(HDIERROR) D PSTHALT("") Q
+ ;
+ S HDIMSG(1)="Post-Installation complete."
+ S HDIMSG(2)=""
+ D MES^XPDUTL(.HDIMSG)
+ Q
+PSTHALT(MSG) ; display error message
+ S HDIMSG(1)=""
+ S HDIMSG(2)=MSG
+ S HDIMSG(3)="***** Post-installation has been halted."
+ S HDIMSG(4)="***** Please contact Enterprise VistA Support."
+ S HDIMSG(5)=""
+ D MES^XPDUTL(.HDIMSG)
+ Q
+DATA ;
+ ;;355.99^.01
+ ;;
+ Q
