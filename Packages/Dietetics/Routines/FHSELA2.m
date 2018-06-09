64f2df68d4a4ff46f10571339d3655771de11ff1
Joseph Snyder <joe.snyder@kitware.com>
2018-06-09 09:13:51 -0400

    Install: FH*5.5*45
    Patch Subject:  ADD ARROWROOT FOOD ALLERGY DISLIKE
    Description:
    
    ============
     This patch will address the addition of a new food allergy by adding it
     into the mapping capability for food dislikes.
     Templates Associated:
     Template Name   Type    File Name (Number)  New/Modified/Deleted
     -------------   ----    ------------------  --------------------
     N/A
     New Service Requests (NSRs):
     ----------------------------
     N/A
     Patient Safety Issues (PSIs):
     -----------------------------
     1380
     Defect Tracking System Ticket(s) & Overview:
     --------------------------------------------
     1.  R18073816FY18 - New Food Allergy
     Problem:
     -------
     The New Term Rapid Turnaround (NTRT) Allergy team has recently added
     ARROWROOT as a food allergy allergies to the GMR ALLERGIES file
     (#120.82). This new food allergy must be added to the routine FHSELA2 in
     order to be mapped as allergy type food preference dislikes in the
     Dietetics package.
     Resolution:
     ----------
     Routine FHSELA2 has a hard coded list of food allergies. It uses this
     hard coded list to present to the user for adding a food allergy
     preference dislike to the FOOD PREFERENCES file (#115.2) and then map it
     to a matching record in the GMR ALLERGIES file (#120.82). ARROWROOT will
     be added to the hard coded list of food allergies in routine FHSELA2 so
     it may be presented to the user.
     Test Sites:
     ----------
     William S. Middleton Memorial Veterans Hospital (Madison)
     VA Western New York HCS
     Patch Installation:
     Pre/Post Installation Overview:
     -------------------------------
    
    Use default answers for KIDS load/install questions.
    
    Patch-Files: http://code.osehra.org/VistA.git/tree/master/Packages/Dietetics/Patches/FH_5.5_45

--- Packages/Dietetics/Routines/FHSELA2.m
+++ Packages/Dietetics/Routines/FHSELA2.m
@@ -1,5 +1,5 @@
 FHSELA2 ;Hines OIFO/RTK - Map GMR Allergy file to Food Prefs ; 3/15/16 11:56am
- ;;5.5;DIETETICS;**8,13,23,25,28,35,37,39,40,41,42,44**;Jan 28, 2005;Build 2
+ ;;5.5;DIETETICS;**8,13,23,25,28,35,37,39,40,41,42,44,45**;Jan 28, 2005;Build 1
  ;
  ;10/9/2009 SLC/GDU FH*5.5*23
  ;  Remedy 317642 FGH - patient food allergy not mapping to N&FS food preferences
@@ -28,6 +28,7 @@ FHSELA2 ;Hines OIFO/RTK - Map GMR Allergy file to Food Prefs ; 3/15/16 11:56am
  ;7/5/2016 SLC/GDU FH*5.5*42 Adding the following food allergies:
  ;  BAY LEAVES, CARAWAY SEEDS, FRUTOSE
  ;5/1/2017 SLC/GDU FH*5.*44 Adding the following food allergy: BEAN SPROUTS
+ ;1/4/2018 DAL/JLC FH*5.5*45 Adding the following food allergy: ARROWROOT
 TMPGL ; Create ^TMP Global
  K ^TMP($J,"FHALG") S FHK=0
  F  S FHK=FHK+1,FHFPS=$T(FPS+FHK),FHZ1=$P(FHFPS,";",3) Q:FHZ1=""  D
@@ -371,3 +372,4 @@ FPS ;;
  ;;CARAWAY SEEDS;CARAWAY SEEDS
  ;;FRUCTOSE;FRUCTOSE
  ;;BEAN SPROUTS;BEAN SPROUTS
+ ;;ARROWROOT;ARROWROOT
