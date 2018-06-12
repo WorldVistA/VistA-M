c32aea107080c14dc43e45af427c5830393481c5
Joseph Snyder <joe.snyder@kitware.com>
2018-06-12 15:40:44 -0400

    Install: PRS*4.0*154
    Patch Subject:  PAID EXPENDITURES REPORT NOT WORKING AFTER VATAS
    Description:
    
    ============
     This patch corrects one issue:
     1. The Personnel and Accounting Integrated Data (PAID) Expenditure
        Report will not work after site migration to the Veterans Affairs Time
        and Attendance System (VATAS).
     Patch Components:
     -----------------
     Files & Fields Associated:
     File Name (Number)      Field Name (Number)     New/Modified/Deleted
     ------------------      -------------------     --------------------
     N/A
     Forms Associated:
     Form Name               File #                  New/Modified/Deleted
     ---------               ------                  --------------------
     N/A
     Mail Groups Associated:
     Mail Group Name         New/Modified/Deleted
     ---------------         --------------------
     N/A
     Options Associated:
     Option Name             Type                    New/Modified/Deleted
     -----------             ----                    --------------------
     N/A
     Protocols Associated:
     Protocol Name           New/Modified/Deleted
     -------------           --------------------
     N/A
     Security Keys Associated:
     Security Key Name
     -----------------
     N/A
     Templates Associated:
     Template Name   Type    File Name (Number)      New/Modified/Deleted
     -------------   ----    ------------------      --------------------
     N/A
     Additional Information:
     N/A
     New Service Requests (NSRs):
     ----------------------------
     N/A
     Patient Safety Issues (PSIs):
     -----------------------------
     N/A
     Defect Tracking System Ticket(s) & Overview:
     --------------------------------------------
     1. I17956679FY18 - PAID - Expenditure Report Issue
     Problem:
     --------
     When a site transitions to VATAS, they do not utilize
     the TIME & ATTENDANCE RECORDS (#458) file. The 'Expenditures'
     [PRSRSU-EXP] [PRSRFI-EXP] reports use the T&L Unit component of the
     '8B Record' (#102) field located in the TIME & ATTENDANCE RECORDS
     (#458) file to sort the results, which are no longer populated.
     Resolution:
     -----------
     Routine PRSROSOR has been modified to use the T&L Unit (#12)
     field of the PAID PAYRUN DATA (#459) file which is still
     used after transition to VATAS.
     Test Sites:
     -----------
     C.W. Bill Young VA Medical Center (Bay Pines)
     Central Arkansas Veterans Healthcare System (Little Rock)
     Documentation Retrieval Instructions:
     -------------------------------------
     Documentation describing the new functionality introduced by this patch is
     available.
     The preferred method is to retrieve files from download.vista.domain.ext.
     This transmits the files from the first available server. Sites may
     also elect to retrieve files directly from a specific server.
     Sites may retrieve the documentation directly using Secure File Transfer
     Protocol (SFTP) from the ANONYMOUS.SOFTWARE directory at the following OI
     Field Offices:
     Hines:  domain.ext
     Salt Lake City: domain.ext
     The documentation will be in the form of Adobe Acrobat files.
     Title                   File Name                     FTP Mode
     --------------------------------------------------------------
     Technical Manual        PAID_4_P154_TM.pdf            Binary
     User Manual             PAID_4_P154_UM.pdf            Binary
     Documentation can also be found on the System Design and Development Web
     page at: http://www.domain.ext/vdl/
     Title                   File Name
     -----------------------------------
     Technical Manual        paid_tm.doc
     Technical Manual        paid_tm.pdf
     User Manual             paid_um.doc
     User Manual             paid_um.pdf
     Patch Installation:
     Pre/Post Installation Overview:
     -------------------------------
     This patch may be installed with users on the system although it is
     recommended it be queued to run after normal business hours to minimize
     potential disruption to users. This patch should take less than 1 minute
     to install.
    
    Use default answers for KIDS load/install questions.
    
    Patch-Files: http://code.osehra.org/VistA.git/tree/master/Packages/PAID/Patches/PRS_4.0_154

--- Packages/PAID/Routines/PRSROSOR.m
+++ Packages/PAID/Routines/PRSROSOR.m
@@ -1,5 +1,5 @@
-PRSROSOR ;HISC/JH-SORT FOR OT/CT & EXPENDITURE REPORTS ;11/18/98
- ;;4.0;PAID;**2,26,46**;Sep 21, 1995
+PRSROSOR ;HISC/JH - SORT FOR OT/CT & EXPENDITURE REPORTS ;11/18/98
+ ;;4.0;PAID;**2,26,46,154**;Sep 21, 1995;Build 2
 OTCT ;Over Time & Comp Time Sort
  S X=$E($P($G(^PRST(458,DA(3),"E",D0,5)),"^"),22,24) D:$P(TLE(1),"^")=X
  .  S TLUNIT=$P($G(^PRSPC(D0,0)),U,7),SSN=$P($G(^(0)),U,9) I SSN'="" S SSN=$E(SSN,1,3)_"-"_$E(SSN,4,5)_"-"_$E(SSN,6,9)
@@ -10,22 +10,22 @@ OTCT ;Over Time & Comp Time Sort
  Q
 EXP ;Expenditure Sort
  S (TL,GOV,STOT,TOT)=0,U="^",TLE=$P(TLE(1),U)
- S DA(3)=$O(^PRST(458,"B",DA(1),0)) Q:DA(3)'>0  D
- .  S D0=0 F  S D0=$O(^PRST(458,DA(3),"E",D0)) Q:D0'>0  S X=$E($P($G(^PRST(458,DA(3),"E",D0,5)),U),22,24) D:TLE=X
- ..;
- ..; skip employee if there is no expenditure data for them
- ..  Q:'($G(^PRST(459,DA,"P",D0,5))!$G(^(8)))
- ..;
- ..  S NAM=$P($G(^PRSPC(D0,0)),U),TOT(1)=$P($G(^PRST(459,DA,"P",D0,5)),U,5),TOT(2)=$P($G(^(5)),U,10),TOT(3)=$P($G(^(5)),U,8),TOT(4)=$P($G(^(5)),U,14)
- ..  S TOT(5)=$P($G(^PRST(459,DA,"P",D0,5)),U,19),TOT(6)=$P($G(^(5)),U,13),TOT(7)=$P($G(^(5)),U,24)+$P($G(^(5)),U,25)+$P($G(^(5)),U,31),TOT(8)=$P($G(^(5)),U,4),TOT(9)=$P($G(^(5)),U,17)
- ..  F I=1:1:9 S TOTAL(I)=TOTAL(I)+TOT(I),$P(STOT,U,I)=$P(STOT,U,I)+TOT(I)
- ..  S TOT=$P($G(^PRST(459,DA,"P",D0,5)),U)
- ..  S GOV(1)=$P($G(^PRST(459,DA,"P",D0,8)),U),GOV=GOV(1)-TOT
- ..  S TOTAL=TOTAL+TOT,TGOV=TGOV+GOV,$P(STOT,U,10)=$P(STOT,U,10)+TOT,$P(STOT,U,11)=$P(STOT,U,11)+GOV,$P(STOT,U,12)=$P(STOT,U,12)+(TOT+GOV)
- ..  S ^TMP($J,"EXP",+$P(DA(1),"-",2),TLE,NAM,D0)=TOT(1)_U_TOT(2)_U_TOT(3)_U_TOT(4)_U_TOT(5)_U_TOT(6)_U_TOT(7)_U_TOT(8)_U_TOT(9)_U_TOT_U_GOV_U_(TOT+GOV)
- .. S CNT=CNT+1,(GOV,TOT)=0
- .. I '(CNT#30),$E(IOST,1,2)="C-",'$D(ZTQUEUED) W "."
- ..  Q
- .  S ^TMP($J,"EXP1",+$P(DA(1),"-",2),TLE)=STOT,STOT=0
- .  Q
+ ;S DA(3)=$O(^PRST(458,"B",DA(1),0)) Q:DA(3)'>0  D
+ ;.  S D0=0 F  S D0=$O(^PRST(458,DA(3),"E",D0)) Q:D0'>0  S X=$E($P($G(^PRST(458,DA(3),"E",D0,5)),U),22,24) D:TLE=X
+ ;*154 - removed checks on 458, no longer used after VATAS. Check t&l in file 459 instead.
+ S D0=0 F  S D0=$O(^PRST(459,DA,"P",D0)) Q:D0'>0  S X=$P($G(^PRST(459,DA,"P",D0,0)),U,13) D:TLE=X 
+ .; skip employee if there is no expenditure data for them
+ . Q:'($G(^PRST(459,DA,"P",D0,5))!$G(^(8)))
+ .;
+ . S NAM=$P($G(^PRSPC(D0,0)),U),TOT(1)=$P($G(^PRST(459,DA,"P",D0,5)),U,5),TOT(2)=$P($G(^(5)),U,10),TOT(3)=$P($G(^(5)),U,8),TOT(4)=$P($G(^(5)),U,14)
+ . S TOT(5)=$P($G(^PRST(459,DA,"P",D0,5)),U,19),TOT(6)=$P($G(^(5)),U,13),TOT(7)=$P($G(^(5)),U,24)+$P($G(^(5)),U,25)+$P($G(^(5)),U,31),TOT(8)=$P($G(^(5)),U,4),TOT(9)=$P($G(^(5)),U,17)
+ . F I=1:1:9 S TOTAL(I)=TOTAL(I)+TOT(I),$P(STOT,U,I)=$P(STOT,U,I)+TOT(I)
+ . S TOT=$P($G(^PRST(459,DA,"P",D0,5)),U)
+ . S GOV(1)=$P($G(^PRST(459,DA,"P",D0,8)),U),GOV=GOV(1)-TOT
+ . S TOTAL=TOTAL+TOT,TGOV=TGOV+GOV,$P(STOT,U,10)=$P(STOT,U,10)+TOT,$P(STOT,U,11)=$P(STOT,U,11)+GOV,$P(STOT,U,12)=$P(STOT,U,12)+(TOT+GOV)
+ . S ^TMP($J,"EXP",+$P(DA(1),"-",2),TLE,NAM,D0)=TOT(1)_U_TOT(2)_U_TOT(3)_U_TOT(4)_U_TOT(5)_U_TOT(6)_U_TOT(7)_U_TOT(8)_U_TOT(9)_U_TOT_U_GOV_U_(TOT+GOV)
+ . S CNT=CNT+1,(GOV,TOT)=0
+ . I '(CNT#30),$E(IOST,1,2)="C-",'$D(ZTQUEUED) W "."
+ . Q
+ S ^TMP($J,"EXP1",+$P(DA(1),"-",2),TLE)=STOT,STOT=0
  Q
