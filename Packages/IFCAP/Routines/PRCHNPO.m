e39a19878b626d4f0b60df179592a8f65edff46d
Joseph Snyder <joe.snyder@kitware.com>
2018-12-02 18:45:20 -0500

    Install: PRC*5.1*208
    Patch Subject:  MODIFY SIMPLIFIED PURCHASE ORDER MAX TO BE $10000
    Description:
    
    ============
     Patch PRC*5.1*208 raises the order limit of micro-purchase threshold for
     goods and services to $10,000 and the single purchase limit for all
     Purchase Cards (PCards) to $10,000 for simplified orders.
     Patch Components:
     -----------------
     Files & Fields Associated:
     File Name (Number)      Field Name (Number)     New/Modified/Deleted
     ------------------      -------------------     --------------------
     N/A
     Forms Associated:
     Form Name               File #                  New/Modified/Deleted
     ---------               ------                  --------------------
     N/A
     Mail Groups Associated:
     Mail Group Name         New/Modified/Deleted
     ---------------         --------------------
     N/A
     Options Associated:
     Option Name             Type                    New/Modified/Deleted
     -----------             ----                    --------------------
     N/A
     Protocols Associated:
     Protocol Name           New/Modified/Deleted
     -------------           --------------------
     N/A
     Security Keys Associated:
     Security Key Name
     -----------------
     N/A
     Templates Associated:
     Template Name   Type    File Name (Number)      New/Modified/Deleted
     -------------   ----    ------------------      --------------------
     N/A
     Additional Information:
     N/A
     New Service Requests (NSRs):
     --------------------------
     N/A
     Patient Safety Issues (PSIs):
     -----------------------------
     N/A
     Defect Tracking System Ticket(s) & Overview:
     --------------------------------------------
     1) INC1350988 - IFCAP - Increase Simplified PCard orders and ancillary
                             requisitions dollar limit to $10000
        Duplicate: INC1494344  - Transaction exceeds purchase card cutoff
        Problem:
        --------
        According to P.L.(Public Law)115-91, the micro purchase threshold may
        be raised up to $10,000 for goods and services, not subject to the
        Service Contract Labor Standards (SCLS) or the Wage Rate Requirements
        (Construction). Each Administrations and Staff Offices can establish
        the micro purchase limit for the individual cardholders up to $10,000.
        Resolution:
        -----------
        Modify the processing checks for order amount in Convert Temporary
        2237 and Simplified Purchase Card orders to raise the maximum cost
        allowed from $3,500 to $10,000.
     Routines: PRCHCON1,PRCHNPO       INPUT TEMPLATE:  PRCHSIMP
     TEST SITES:
     -----------
     Hudson Valley HCS
     Software and Documentation Retrieval Instructions:
     --------------------------------------------------
     N/A
     Patch Installation:
     Pre/Post Installation Overview:
     -------------------------------
     You may install this patch at any time without interruption to users. It
     should take less than 2 minutes to install this patch.
    
    Use default answers for KIDS load/install questions.
    
    Patch-Files: http://code.osehra.org/VistA.git/tree/master/Packages/IFCAP/Patches/PRC_5.1_208

--- Packages/IFCAP/Routines/PRCHNPO.m
+++ Packages/IFCAP/Routines/PRCHNPO.m
@@ -1,5 +1,5 @@
-PRCHNPO ;WISC/SC,ID/RSD/RHD/DGL/BGJ-ENTER NEW PURCHASE ORDER/REQUISITION ; 4/2/01 1:50pm
-V ;;5.1;IFCAP;**7,11,79,108,123,184,192**;Oct 20, 2000;Build 3
+PRCHNPO ;WISC/SC,ID/RSD/RHD/DGL/BGJ-ENTER NEW PURCHASE ORDER/REQUISITION ;4/2/01 1:50pm
+V ;;5.1;IFCAP;**7,11,79,108,123,184,192,208**;Oct 20, 2000;Build 1
  ;Per VA Directive 6402, this routine should not be modified.
  ;
  ;PRC*5.1*184 Added check for Purchase Card orders to insure there
@@ -7,7 +7,7 @@ V ;;5.1;IFCAP;**7,11,79,108,123,184,192**;Oct 20, 2000;Build 3
  ;            requistion created in file 410 for realted FCP used
  ;            and control for Running Balance Report.
  ;
- ;PRC*5.1*192 Changed order limit in opening mesdsage to $3500 at tag MSG
+ ;PRC*5.1*208 Changed order limit in opening message to $10000 at tag MSG
  ;
  S NOTCOMPL=0 ;Initialize for Incomplete Template.
  D SWITCH^PRCHUTL K ERRFLG ; SET LOG/ISMS SWITCH
@@ -152,7 +152,7 @@ MSG ;Call by the "ENTRY ACTION" for Simplified PC (PRC*5.1*79)
  NEW MSG
  S MSG(1)="*********************************************"
  S MSG(1,"F")="!!?15"
- S MSG(2)="*  IF THE ORDER IS MORE THAN $3500.00       *"     ;PRC*5.1*192
+ S MSG(2)="*  IF THE ORDER IS MORE THAN $10000.00      *"     ;PRC*5.1*208
  S MSG(2,"F")="!?15"
  S MSG(3)="*  OR IS ON A CONTRACT, YOU CANNOT USE      *"
  S MSG(3,"F")="!?15"
