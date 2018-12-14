c64deca846869c6608e3f4792c3005249448a538
Sam Habiel <sam.habiel@gmail.com>
2018-12-14 12:35:34 -0500

    Data Translation Framework + Sample Translation
    These changes allow translation of unindexed data anywhere in Fileman.
    
    Changes:
    0.81+DATA TYPE.zwr: DIALOG data type for Use anywhere in Fileman Files
    101.24: DD for HEADING field & Data
    0.84 for the translations
    ORWCV1 modified to use a standard Fileman API to get the data

--- Packages/Order Entry Results Reporting/Routines/ORWCV1.m
+++ Packages/Order Entry Results Reporting/Routines/ORWCV1.m
@@ -1,5 +1,7 @@
-ORWCV1 ; slc/dcm - CoverSheet calls continued ;12:30 PM  4 Oct 2000
- ;;3.0;ORDER ENTRY/RESULTS REPORTING;**85**;Dec 17, 1997
+ORWCV1 ; slc/dcm - CoverSheet calls continued ;Nov 24, 2018@17:07
+ ;;3.0;ORDER ENTRY/RESULTS REPORTING;**85,OSE/SMH**;Dec 17, 1997
+ ; OSE/SMH Changes (c) Sam Habiel 2018
+ ; Licensed Under Apache 2.0
  ;
 COVERLST(LST)        ; -- return data for coversheet
  ;RPC: ORWCV1 COVERSHEET LIST
@@ -10,7 +12,7 @@ COVERLST(LST)        ; -- return data for coversheet
  . Q:$P(X0,"^",12)="L"
  . S RPC=$P($G(^XWB(8994,+$P(X0,"^",13),0)),"^"),DETAIL=""
  . I $P(X0,"^",18) S DETAIL=$P($G(^ORD(101.24,+$P(X0,"^",18),0)),"^",13),DETAIL=$P($G(^XWB(8994,+DETAIL,0)),"^")
- . S HEAD=$P(X0,"^") I $L($P(X2,"^",3)) S HEAD=$P(X2,"^",3)
+ . S HEAD=$P(X0,"^") I $L($P(X2,"^",3)) S HEAD=$$GET1^DIQ(101.24,$P(LIST(I),U,2),.23) ; OSE/SMH - was S HEAD=$P(X2,"^",3)
  . S X=$P(X0,"^",2)_"^"_HEAD_"^"_$P(X0,"^",3)_"^"_$P(X0,"^",12)_"^"_$P(X0,"^",7)_"^"_RPC_"^"_$P(X0,"^",9)
  . S X=X_"^"_$P(X0,"^",10)_"^"_$P(X0,"^",11)_"^"_$P(X0,"^",14)_"^"_$P(X0,"^",15)_"^"_$P(X2,"^")_"^"_$P(X0,"^",4)_"^"_$P(X0,"^",16)_"^"_$P(X0,"^",17)_"^"_DETAIL_"^"_LIST(I)
  . D SETITEM(.ROOT,X)
