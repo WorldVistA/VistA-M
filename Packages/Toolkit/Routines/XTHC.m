cde568c5300895236d6f2c6fe7b7f29ac01dbac1
Joseph Snyder <joe.snyder@kitware.com>
2019-02-22 11:17:24 -0500

    Install: XT*7.3*10002
    Patch Subject: None
    Description:
    
    Use default answers for KIDS load/install questions.
    
    Patch-Files: http://code.osehra.org/VistA.git/tree/master/Packages

--- Packages/Toolkit/Routines/XTHC.m
+++ Packages/Toolkit/Routines/XTHC.m
@@ -1,5 +1,10 @@
-XTHC ;HCIOFO/SG - HTTP 1.0 CLIENT ;11/10/2009
- ;;7.3;TOOLKIT;**123**;Apr 25, 1995;Build 4
+XTHC ;HCIOFO/SG - HTTP 1.0 CLIENT ; 12/5/18 8:02pm
+ ;;7.3;TOOLKIT;**123,10002**;Apr 25, 1995;Build 1
+ ;
+ ; *10002* changes (c) Sam Habiel 2015-2018
+ ; See repository for license terms.
+ ; *10002* - TLS/libcurl on GTM/YDB support
+ ; ALL CODE IS NEW. Previously routine only had comments.
  ;
  ; API ENTRY POINTS ---- DESCRIPTIONS
  ;
@@ -21,3 +26,79 @@ XTHC ;HCIOFO/SG - HTTP 1.0 CLIENT ;11/10/2009
  ;  $$REQUEST^XTHC10A    Sends the HTTP request
  ;
  Q
+ ;
+TEST D EN^%ut($T(+0),3) QUIT
+ ;
+TGET1 ; @TEST GET via TLS
+ N SSS,XXX
+ N STATUS S STATUS=$$GETURL^XTHC10("https://httpbin.org/stream/20",1,$NA(SSS),.XXX)
+ D CHKTF^%ut(+STATUS=200)
+ N CNT S CNT=0
+ N I F I=0:0 S I=$O(SSS(I)) Q:'I  I SSS(I)]"" S CNT=CNT+1
+ D CHKTF^%ut(CNT=20)
+ QUIT
+ ;
+TGET2 ; @TEST GET example.com
+ N STATUS,SSS,XXX S STATUS=$$GETURL^XTHC10("https://example.com",1,$NA(SSS))
+ D CHKTF^%ut(+STATUS=200)
+ QUIT
+ ;
+TPOST ; @TEST Test Post
+ N PAYLOAD,RTN,H,RET
+ N R S R=$R(123423421234)
+ S PAYLOAD(1)="KBANTEST ; VEN/SMH - Test routine for Sam ;"_R
+ S PAYLOAD(2)=" QUIT"
+ N STATUS S STATUS=$$GETURL^XTHC10("https://httpbin.org/post",1,$NA(RTN),.H,$NA(PAYLOAD))
+ D CHKTF^%ut(+STATUS=200)
+ N DATALINE N I F I=0:0 S I=$O(RTN(I)) Q:'I  I RTN(I)["""data""" S DATALINE=RTN(I)
+ D CHKTF^%ut($G(DATALINE)[R)
+ QUIT
+ ;
+TESTH ; @TEST Unit Test with headers
+ N RTN
+ N HEADER
+ S HEADER("DNT")=1
+ N STATUS S STATUS=$$GETURL^XTHC10("https://httpbin.org/headers",1,$NA(RTN),,,.HEADER)
+ N OK S OK=0
+ N I F I=0:0 S I=$O(RTN(I)) Q:'I  I $$UP^XLFSTR(RTN(I))["DNT" S OK=1
+ D CHKTF^%ut(+STATUS=200,"Status code is supposed to be 200")
+ D CHKTF^%ut(OK,"Couldn't get the sent header back")
+ QUIT
+ ;
+TMI ; @TEST Multiple GETs from Single Domain - Init
+ D INIT^XTHC10(0) ; 0 = Don't auto close
+ quit
+TM1 ; @TEST Multiple GETs from Single Domain - First
+ n sss,zzz,status
+ s status=$$GETURL^XTHC10("https://rxnav.nlm.nih.gov/REST/ndcstatus.json?ndc=00143314501",1,$NA(sss))
+ d CHKEQ^%ut(+status,200)
+ quit
+TM2 ; @TEST Multiple GETs from Single Domain - Second
+ n sss,zzz,status
+ s status=$$GETURL^XTHC10("https://rxnav.nlm.nih.gov/REST/drugs?name=cymbalta",1,$NA(sss))
+ d CHKEQ^%ut(+status,200)
+ quit
+TM3 ; @TEST Multiple GETs from Single Domain - Third
+ n sss,zzz,status
+ s status=$$GETURL^XTHC10("https://rxnav.nlm.nih.gov/REST/termtypes",1,$NA(sss))
+ d CHKEQ^%ut(+status,200)
+ quit
+TM4 ; @TEST Multiple GETs from Single Domain - Fourth
+ n sss,zzz,status
+ s status=$$GETURL^XTHC10("https://rxnav.nlm.nih.gov/REST/brands?ingredientids=8896+20610",1,$NA(sss))
+ d CHKEQ^%ut(+status,200)
+ quit
+TM5 ; @TEST Multiple GETs from Single Domain - Fifth
+ n sss,zzz,status
+ s status=$$GETURL^XTHC10("https://rxnav.nlm.nih.gov/REST/brands?ingredientids=8896+20610",1,$NA(sss))
+ d CHKEQ^%ut(+status,200)
+ quit
+TM6 ; @TEST Mulitple GETs from Single Domain - Sixth
+ n sss,zzz,status
+ s status=$$GETURL^XTHC10("https://rxnav.nlm.nih.gov/REST/approximateTerm?term=zocor%2010%20mg&maxEntries=4",1,$NA(sss))
+ d CHKEQ^%ut(+status,200)
+ quit
+TMC ; @TEST Multiple GETs from Single Domain - Cleanup
+ d CLEANUP^XTHC10
+ quit
+ ;
--- Packages/Toolkit/Routines/XTHC10.m
+++ Packages/Toolkit/Routines/XTHC10.m
@@ -1,19 +1,20 @@
-XTHC10 ;HCIOFO/SG - HTTP 1.0 CLIENT ;12/07/2011
- ;;7.3;TOOLKIT;**123,566**;Apr 25, 1995;Build 6
+XTHC10 ;HCIOFO/SG - HTTP 1.1 CLIENT ; 12/5/18 8:45pm
+ ;;7.3;TOOLKIT;**123,566,10002**;Apr 25, 1995;Build 1
+ ; *10002 Changes: TLS Support by Sam Habiel (c) 2018
+ ; passim changes
  ;
  Q
- ;
- ;##### GETS THE DATA FROM THE PROVIDED URL USING HTTP 1.0
+ ;##### GETS THE DATA FROM THE PROVIDED URL USING HTTP 1.1
  ;
  ; URL           URL (http://host:port/path)
  ;
- ; [XT8FLG]      Timeout and flags to control processing.
- ;               If a value of  this parameter starts from a number
+ ; [XT8FLG]      (Optional) Timeout and flags to control processing.
+ ;               If a value of this parameter starts with a number
  ;               then this number is used as a value of the timeout
  ;               (in seconds). Otherwise, the default value of 5
  ;               seconds is used.
  ;
- ; [XT8RDAT]     Closed root of the variable where the message
+ ; [XT8RDAT]     (Optional) Closed root of the variable where the message
  ;               body is returned. Data is stored in consecutive
  ;               nodes (numbers starting from 1). If a line is
  ;               longer than 245 characters, only 245 characters
@@ -31,145 +32,222 @@ XTHC10 ;HCIOFO/SG - HTTP 1.0 CLIENT ;12/07/2011
  ;                 @XT8DATA@(6)="</p>"
  ;                 ...
  ;
- ; [.XT8RHDR]    Reference to a local variable where the parsed
+ ; [.XT8RHDR]    (Optional) Reference to a local variable where the parsed
  ;               headers are returned. Header names are converted to
- ;               upper case; the values are left "as is". The root
+ ;               upper case and the values are left "as is". The root
  ;               node contains the status line. For example:
  ;
- ;                 XT8HDR="HTTP/1.0 200 OK"
- ;                 XT8HDR("CACHE-CONTROL")="private"
- ;                 XT8HDR("CONNECTION")="Keep-Alive"
- ;                 XT8HDR("CONTENT-LENGTH")="2690"
- ;                 XT8HDR("CONTENT-TYPE")="text/html"
- ;                 XT8HDR("DATE")="Fri, 26 Sep 2003 16:04:10 GMT"
- ;                 XT8HDR("SERVER")="GWS/2.1"
+ ;                 XT8HDR="HTTP/1.1 200 OK"
+ ;                 XT8HDR("ACCEPT-RANGES")="bytes"
+ ;                 XT8HDR("CONNECTION")="close"
+ ;                 XT8HDR("CONTENT-LENGTH")="16402"
+ ;                 XT8HDR("CONTENT-TYPE")="text/html; charset=UTF-8"
+ ;                 XT8HDR("DATE")="Thu, 25 Jun 2015 14:43:01 GMT"
+ ;                 XT8HDR("ETAG")="a93a2-4012-5180156550680"
+ ;                 XT8HDR("LAST-MODIFIED")="Mon, 08 Jun 2015 13:08:26 GMT"
+ ;                 XT8HDR("SERVER")="Apache/2.2.15 (CentOS)"
  ;
- ; [XT8SDAT]     Closed root of a variable containing body of the
- ;               request message. Data should be formatted as
- ;               described earlier (see the XT8RDAT parameter).
+ ; [XT8SDAT]     (Optional) Reference to a local variable containing the
+ ;               request message body. Data should be formatted as in
+ ;               variable XT8RDAT above.
  ;
- ;               NOTE: If this parameter is defined, not empty, and
- ;                     the referenced array contains data then the
- ;                     POST request is generated. Otherwise, the GET
- ;                     request is sent.
- ;
- ; [.XT8SHDR]    Reference to a local variable containing header
+ ; [.XT8SHDR]    (Optional) Reference to a local variable containing header
  ;               values, which will be added to the request.
+ ;                 XT8SHDR("CONTENT-TYPE")="text/html"
  ;
- ; [REDIR]       This IS NOT a published parameter. It is used
- ;               internally to limit number of redirections.
+ ; [XT8METH]     (Optional) Flag to indicate the request method.
+ ;                    "GET"     - Default if XT8SDAT contains no data
+ ;                    "POST"    - Default if XT8SDAT contains data
+ ;                    "HEAD"
+ ;                    "PUT"
+ ;                    "OPTIONS"
+ ;                    "DELETE"
+ ;                    "TRACE"
  ;
  ; Return values:
  ;
- ;           <0  Error Descriptor (see the $$ERROR^XTERROR)
+ ;           <0  Error Descriptor
  ;           >0  HTTP Status Code^Description
  ;
- ;    Most common HTTP status codes:
- ;
- ;          200  Ok
- ;
- ;          301  Moved Permanently (The application should either
- ;               automatically update the URL with the new one from
- ;               the Location response header or instruct the user
- ;               how to do this).
- ;
- ;          302  Moved Temporarily (The application should continue
- ;               using the original URL).
- ;
- ;               NOTE: You will not see this code for GET requests.
- ;                     They are redirected automatically.
- ;
- ;          303  See Other (The resource has moved to another URL
- ;               given by the Location response header, and should
- ;               be automatically retrieved by the client using the
- ;               GET method. This is often used by a CGI script to
- ;               redirect the client to an existing file).
- ;
- ;               NOTE: You will not see this status code because it
- ;                     is handled automatically inside the function.
- ;
+ ;    Common HTTP status codes returned:
+ ;          200  OK
+ ;          301  Moved Permanently
  ;          400  Bad Request
- ;
+ ;          401  Unauthorized
+ ;          402  Payment Required
+ ;          403  Forbidden
  ;          404  Not Found
+ ;          405  Method Not Allowd
+ ;          406  Not Acceptable
+ ;          407  Proxy Authentication Required
+ ;          408  Request Time-out
+ ;          500  Internal Server Error
+ ;          501  Not Implemented
+ ;          502  Bad Gateway
+ ;          503  Service Unavailable
+ ;          504  Gateway Time-out
+ ;          505  HTTP Version not supported
  ;
- ;          500  Server Error (An unexpected server error. The most
- ;               common cause is a server-side script that has bad
- ;               syntax, fails, or otherwise can't run correctly).
+ ; See: www.ietf.org/rfc/rfc2616.txt (HTTP/1.1)
+ ;      www.ieft.org/rfc/rfc2617.txt (HTTP Authentication)
  ;
- ; See the http://www.faqs.org/rfcs/rfc1945.html for more details.
+INIT(AUTOCLOSE) ;
+ ; Supply Autoclose if you want the code to automatically close the connection
+ K ^TMP("XTHC10",$J)
+ I ^%ZOSF("OS")["GT.M" D &libcurl.init
+ I ^%ZOSF("OS")["OpenM" S XT8REQUEST=##class(%Net.HttpRequest).%New()
+ S ^TMP("XTHC10",$J)=AUTOCLOSE
+ QUIT
  ;
-GETURL(URL,XT8FLG,XT8RDAT,XT8RHDR,XT8SDAT,XT8SHDR,REDIR) ;
- N HOST,I,IP,IPADDR,PATH,PORT,RQS,STATUS,X
- ;**P566 START CJM
- N IO,POP
- ;**P566 END CJM
+CLEANUP ;
+ I ^%ZOSF("OS")["GT.M" D &libcurl.cleanup
+ I ^%ZOSF("OS")["OpenM" K XT8REQUEST
+ K ^TMP("XTHC10",$J)
+ QUIT
+ ;
+GETURL(URL,XT8FLG,XT8RDAT,XT8RHDR,XT8SDAT,XT8SHDR,XT8METH) ;IA# 5553
+ ;ZEXCEPT: %Net,%New,ContentType,HttpRequest,Https,Location,OpenTimeout,Port,Server,SSLCheckServerIdentity,SSLConfiguration,class   ; Cache ObjectScript methods and properties
+ ;ZEXCEPT: Data,Get,GetHeader,GetNextHeader,Head,HttpResponse,Post,Put,ReadLine,ReasonPhrase,Send,SocketTimeout,StatusCode,StatusLine    ; Cache ObjectScript methods and properties
+ ;ZEXCEPT: XT8REQUEST
+ N EOL,ERR,ESTATUS,HOST,I,J,K,NEWLINE,PATH,PORT,RESPONSE,STATUS,X,Y
  S XT8FLG=$G(XT8FLG)  S:XT8FLG'?1.N.E XT8FLG="5"_XT8FLG
  ;
  ;Check IO
  I '$D(IO(0)) D HOME^%ZIS
- ;=== Parse the URL
+ S STATUS=0
+ ;
+ I '$D(^TMP("XTHC10",$J)) D INIT(1)
+ I ^%ZOSF("OS")["GT.M" G GTM
+ ;
+CACHE ;
  S I=$$PARSEURL^XTHCURL(URL,.HOST,.PORT,.PATH)  Q:I<0 I
- ;
- ;=== Check the host name/address
- I HOST'?1.3N3(1"."1.3N)  D  Q:IPADDR="" $$ERROR(2,HOST)
- . ;--- Resolve the host name into the IP address(es)
- . S IPADDR=$$ADDRESS^XLFNSLK(HOST)  Q:IPADDR=""
- . ;--- Check for the Host header value
+ I $G(PORT)>0 S XT8REQUEST.Port=PORT ;Set port here, reset to 443 if https
+ I $$UP^XLFSTR($E(URL,1,8))["HTTPS://" D
+ . S XT8REQUEST.Https=1
+ . S XT8REQUEST.SSLCheckServerIdentity=0
+ . S XT8REQUEST.SSLConfiguration="encrypt_only"
+ . I $G(PORT)=80 S XT8REQUEST.Port=443 ;If default http port, reset to default https
+ S XT8REQUEST.Server=HOST
+ I $G(PATH)'="" S XT8REQUEST.Location=PATH
+ S XT8REQUEST.ContentType="text/html"
+ S XT8REQUEST.OpenTimeout=+XT8FLG
+ S XT8REQUEST.SocketTimeout=0
+ ;Set custom headers
+ D XT8REQUEST.SetHeader("User-Agent","VistA/2.0")
+ S I=""
+ F  S I=$O(XT8SHDR(I))  Q:I=""  D XT8REQUEST.SetHeader(I,XT8SHDR(I))
+ ; "GET"
+ I $G(XT8METH)="GET" D
+ . S ESTATUS=XT8REQUEST.Get()
+ ; "POST"
+ I $G(XT8METH)="POST" D
  . S I=""
- . F  S I=$O(XT8SHDR(I))  Q:(I="")!($$UP^XLFSTR(I)="HOST")
- . S:I="" XT8SHDR("Host")=HOST
- E  S IPADDR=HOST
+ . F  S I=$O(@XT8SDAT@(I))  Q:I=""  D  ;load an entire page, not just key/value pairs
+ . . S NEWLINE=$G(@XT8SDAT@(I))
+ . . S J=""
+ . . F  S J=$O(@XT8SDAT@(I,J))  Q:J=""  S NEWLINE=NEWLINE_$G(@XT8SDAT@(I,J))
+ . . D XT8REQUEST.EntityBody.WriteLine(NEWLINE)
+ . S ESTATUS=XT8REQUEST.Post()
+ ; "HEAD"
+ I $G(XT8METH)="HEAD" D
+ . S ESTATUS=XT8REQUEST.Head()
+ ; "PUT"
+ I $G(XT8METH)="PUT" D
+ . S I=""
+ . F  S I=$O(@XT8SDAT@(I))  Q:I=""  D  ;load an entire page, not just key/value pairs
+ . . S NEWLINE=$G(@XT8SDAT@(I))
+ . . S J=""
+ . . F  S J=$O(@XT8SDAT@(I,J))  Q:J=""  S NEWLINE=NEWLINE_$G(@XT8SDAT@(I,J))
+ . . D XT8REQUEST.EntityBody.WriteLine(NEWLINE)
+ . S ESTATUS=XT8REQUEST.Put()
+ ; "OPTIONS" "DELETE" "TRACE"
+ I $D(XT8METH) D
+ . F  S I=$O(@XT8SDAT@(I))  Q:I=""  D  ;load an entire page, not just key/value pairs
+ . . S NEWLINE=$G(@XT8SDAT@(I))
+ . . S J=""
+ . . F  S J=$O(@XT8SDAT@(I,J))  Q:J=""  S NEWLINE=NEWLINE_$G(@XT8SDAT@(I,J))
+ . . D XT8REQUEST.EntityBody.WriteLine(NEWLINE)
+ . S ESTATUS=XT8REQUEST.Send($G(XT8METH))
+ ;Defaults if XT8METH is not defined
+ ;If XT8SDAT has data, then do POST method, otherwise GET
+ I $D(XT8SDAT)>0 D
+ . S I=""
+ . F  S I=$O(@XT8SDAT@(I))  Q:I=""  D  ;load an entire page, not just key/value pairs
+ . . S NEWLINE=$G(@XT8SDAT@(I))
+ . . S J=""
+ . . F  S J=$O(@XT8SDAT@(I,J))  Q:J=""  S NEWLINE=NEWLINE_$G(@XT8SDAT@(I,J))
+ . . D XT8REQUEST.EntityBody.WriteLine(NEWLINE)
+ . S ESTATUS=XT8REQUEST.Post()
+ E  D
+ . S ESTATUS=XT8REQUEST.Get()
+ I 'ESTATUS D $system.Status.DecomposeStatus(ESTATUS,.ERR) Q "-1^"_ERR(1)  ;Q if errors
  ;
- ;=== Connect to the host
- F I=1:1  S IP=$P(IPADDR,",",I)  Q:IP=""  D  Q:'$G(POP)
- . D CALL^%ZISTCP(IP,PORT,+XT8FLG)
- Q:$G(POP) $$ERROR(3,IPADDR)
+ S RESPONSE=XT8REQUEST.HttpResponse ;Sets the %Net.HttpResponse object
+ S STATUS=RESPONSE.StatusCode_"^"_RESPONSE.ReasonPhrase
+ ;--- Header - Enter header into XT8RHDR
+ S XT8RHDR=RESPONSE.StatusLine
+ S I=""
+ F  D  Q:I=""
+ . S I=RESPONSE.GetNextHeader(I) Q:I=""  ;Name of header
+ . S XT8RHDR(I)=RESPONSE.GetHeader(I)    ;Value of header I
+ ;--- Data - Read stream one line at a time and enter into XT8RDAT
+ F J=1:1 D  Q:(RESPONSE.Data.AtEnd)!(+STATUS=-1)  ;quit if at end of data stream or error
+ . S ESTATUS="" ;Status object
+ . S X=RESPONSE.Data.ReadLine(,.ESTATUS,.EOL)
+ . I 'ESTATUS D
+ . . D $system.Status.DecomposeStatus(ESTATUS,.ERR) S STATUS="-1^"_ERR(1)  ;Q if errors
+ . E  I $D(XT8RDAT)>0 D
+ . . S @XT8RDAT@(J)=$E(X,1,245)  ;first 245 chars into parent node (J)
+ . . I $L(X)>245 D               ;append remaining chars into child nodes (J,K), 245 chars at a time
+ . . . F K=1:1:($L(X)\245) D
+ . . . S Y=$E(X,(K*245)+1,(K*245)+245) Q:Y=""
+ . . . S @XT8RDAT@(J,K)=Y
  ;
- ;=== Perform the transaction
- D
- . N $ESTACK,$ETRAP
- . ;--- Setup the error processing
- . ;D SETDEFEH^XTERROR("STATUS")
- . S $ET="D ETRAP^XTHC10"
- . ;--- Send the request and get the response
- . S RQS=$$REQUEST^XTHC10A(PATH,$G(XT8SDAT),.XT8SHDR)
- . I RQS<0  S STATUS=RQS  Q
- . S STATUS=$$RECEIVE^XTHC10A(+XT8FLG,$G(XT8RDAT),.XT8RHDR)
- ;
- ;=== Close the socket
- D CLOSE^%ZISTCP
- ;
- ;=== Redirect if requested by the server
- S I=+STATUS
- I (I\100)=3  D:$S(I=303:1,I=301:0,1:RQS="GET")
- . I $G(REDIR)>5  S STATUS=$$ERROR(5)  Q
- . S URL=$G(XT8RHDR("LOCATION"))
- . ;I URL=""  S STATUS=$$ERROR^XTERROR(-150000.024)  Q
- . I URL=""  S STATUS=$$ERROR(4)  Q
- . I RQS="POST"  N XT8SDAT  ; Force the GET request
- . S STATUS=$$GETURL(URL,XT8FLG,$G(XT8RDAT),.XT8RHDR,$G(XT8SDAT),.XT8SHDR,$G(REDIR)+1)
- ;
- ;=== Return the status
- ;I +STATUS=-150000.004  S X=$$LASTERR^XTERROR1()  S:X STATUS=X
- I +STATUS=-6 S STATUS=STATUS("ERROR")
+ I ^TMP("XTHC10",$J) D CLEANUP
  Q STATUS
  ;
-ETRAP ;Catch a runtime error
- N EC
- S STATUS("ERROR")=$$EC^%ZOSV D ^%ZTER
- S STATUS=-6
- I $L($EC) S $ECODE="" S $ETRAP="D UNW^%ZTER Q:$QUIT STATUS Q  " S $ECODE=",U1,"
- Q
+GTM ;
+ ; ZEXCEPT: STATUS,XT8RDAT,XT8RHDR,XT8SDAT,XT8SHDR,I,J,K,Y
+ ; URL = Self explanatory
+ ; XT8FLG = Timeout
+ ; XT8RDAT = Return Data
+ ; XT8RHDR = Return Headers
+ ; XT8SDAT = Input Message
+ ; XT8SHDR = Input Headers
+ ; XT8METH = Method
  ;
-ERROR(ENUM,PARAM) ;Expand error
- N MSG
- S MSG=$P($T(@ENUM),";;",2) S:MSG["|" MSG=$P(MSG,"|")_$G(PARAM)_$P(MSG,"|",2)
- Q MSG
+ ; Process Payload
+ N XT8PAYLOAD S XT8PAYLOAD=""
+ I $D(XT8SDAT)>0 D
+ . S I=""
+ . F  S I=$O(@XT8SDAT@(I))  Q:I=""  D
+ . . S XT8PAYLOAD=XT8PAYLOAD_$G(@XT8SDAT@(I))
+ . . S J=""
+ . . F  S J=$O(@XT8SDAT@(I,J))  Q:J=""  S XT8PAYLOAD=XT8PAYLOAD_$G(@XT8SDAT@(I,J))
  ;
-1 ;;-1^Missing host name.
-2 ;;-1^Cannot resolve the host name: |.
-3 ;;-1^Cannot connect to host.
-4 ;;-1^Missing redirection URL.
-5 ;;-1^Too many redirections.
-6 ;;-6^Run Time Error.
-7 ;;-1^Time Out.
+ ; Input Headers
+ S I=""
+ F  S I=$O(XT8SHDR(I))  Q:I=""  d &libcurl.addHeader(I_": "_XT8SHDR(I))
+ ;
+ ; Make Call
+ N OUTPUT,HEADERS
+ K STATUS
+ d &libcurl.do(.STATUS,.OUTPUT,$G(XT8METH),URL,XT8PAYLOAD,"application/json",+$G(XT8FLG,5),.HEADERS)
+ ;
+ ; Unwrap headers
+ N CRLF S CRLF=$C(13,10)
+ N I F I=1:1:$L(HEADERS,CRLF) S XT8RHDR(I)=$P(HEADERS,CRLF,I) I XT8RHDR(I)="" K XT8RHDR(I)
+ ;
+ ; Unwrap Output
+ F I=1:1:$L(OUTPUT,$C(10)) D             ; Unwrap by LF
+ . N X S X=$P(OUTPUT,$C(10),I)           ; Make it easy to manipulate
+ . I $E(X,$L(X))=$C(13) S $E(X,$L(X))="" ; Remove CR if there
+ . S @XT8RDAT@(I)=$E(X,1,245)            ; Extract 1-245
+ . I $L(X)>245 D                         ; Get chars after 245
+ .. F K=1:1:($L(X)\245) D
+ .. S Y=$E(X,(K*245)+1,(K*245)+245) Q:Y=""
+ .. S @XT8RDAT@(I,K)=Y
+ ;
+ ; Last bits: cleanup and return
+ I ^TMP("XTHC10",$J) D CLEANUP
+ Q STATUS
