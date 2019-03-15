fe4a4b81ab661eb8e707c05903a21aa9ce93817b
Joseph Snyder <joe.snyder@kitware.com>
2019-03-15 06:43:17 -0400

    Install: XU*8.0*705
    Patch Subject:  KERNEL VISTA ENHANCEMENTS - MVI NEW PERSON STAT REPORT
    Description:
    
    ============
     There are NO required builds for patch XU*8.0*705.
     The following enhancement exported in this patch was requested by and
     in support of the Master Veteran Index (MVI) application for retrieval
     of statistics in the NEW PERSON (#200) file that will provide essential
     information after Enterprise User Identity functionality has been
     incorporated/released in subsequent KERNEL/MVI patches.
     Enhancement
     A new restricted Remote Procedure Call (RPC) [XUS MVI NEW PERSON DATA]
     was created to give the MVI system the ability to remotely retrieve the
     following '^' delimited aggregated data for Active and/or Non-Active
     NEW PERSON (File #200) entries from a 'specific' or 'all' active VistA
     site(s) if requested:
          - Number of ALL Record types found
          - Number of Non-Active Records
          - Number of Active Records
          - Number of Active Records containing a SECID
          - Number of Active Records containing an Email
          - Number of Active Records containing an Network Username
          - Number of Visitor Records
     Patch Components
     ================
     Files & Fields Associated:
     File Name (Number)       Field Name (Number)       New/Modified/Deleted
     ------------------       -------------------       --------------------
     N/A
     Forms Associated:
     Form Name             File #       New/Modified/Deleted
     ---------             ------       --------------------
     N/A
     Mail Groups Associated:
     Mail Group Name          New/Modified/Deleted
     ---------------          --------------------
     N/A
     Options Associated:
     Option Name            Type               New/Modified/Deleted
     -----------            ----               --------------------
     N/A
     Protocols Associated:
     Protocol Name       New/Modified/Deleted
     -------------       --------------------
     N/A
     Security Keys Associated:
     Security Key Name
     -----------------
     N/A
     Templates Associated:
     Template Name      Type      File Name (Number)  New/Modified/Deleted
     -------------      ----      ------------------  --------------------
     N/A
     Remote Procedures Associated:
     Remote Procedure Name             New/Modified/Deleted
     ---------------------------       --------------------
     XUS MVI NEW PERSON DATA           NEW
     Parameter Definitions Associated:
     Parameter Name                    New/Modified/Deleted
     ---------------------------       --------------------
     N/A
     Additional Information:
     Blood Bank Team Coordination
     ----------------------------
     EFFECT ON BLOOD BANK FUNCTIONAL REQUIREMENTS: Patch XU*8*705 contains
     changes to a package referenced in ProPath standard titled: BBM Team
     Review of VistA Patches. This patch does not alter or modify any VistA
     Blood Bank software design safeguards or safety critical elements
     functions.
     RISK ANALYSIS: Changes made by patch XU*8*705 have no adverse effect on
     Blood Bank software functionality, therefore RISK is none.
     New Service Requests (NSRs)
     ----------------------------
     N/A
     Patient Safety Issues (PSIs)
     -----------------------------
     N/A
     Defect Tracking System Ticket(s) & Overview
     -------------------------------------------
     N/A
     Test Sites:
     ----------
     CENTRAL ALABAMA HCS
     WILLIAM S. MIDDLETON VAMC (MADISON WI)
     Patch Installation:
     Pre/Post Installation Overview
     ------------------------------
     There are no pre/post installation routines in this patch.
    
    Use default answers for KIDS load/install questions.
    
    Patch-Files: http://code.osehra.org/VistA.git/tree/master/Packages/Kernel/Patches/XU_8.0_705

---
+++ Packages/Kernel/Routines/XUMVIDTA.m
@@ -0,0 +1,82 @@
+XUMVIDTA ;MVI/CKN - MVI New Person Data Analysis ; 10/9/18 9:13am
+ ;;8.0;KERNEL;**705**;Jul 10, 1995;Build 1
+ Q
+EP(RETURN,SEL,ACTSEL) ;
+ ;RPC - XUS MVI NEW PERSON DATA
+ ;This RPC is called by MPI to get data analysis report from
+ ;NEW PERSON file (#200) for active VistA sites
+ ;Input:
+ ;    SEL (Required) - 1 : All - Active New Person and  Non Active New Person
+ ;                     2 : Active New Person
+ ;                     3 : Non Active New Person
+ ; ACTSEL (Optional for Non Active) - 1 : All
+ ;                     1 : All
+ ;                     2 : Active records having a SecID value
+ ;                     3 : Active records having an Email value
+ ;                     4 : Active records having a NT USERNAME value
+ ;Output:
+ ; RETURN - Total Count^Total Non Active^Total Active^Total SECID^Total Email
+ ;          ^Total NT User^Total Visitors
+ ;
+ N XUDUZ,U,TOTVIS,TOTCNT,NONACT,ACT,TOTNACT,TOTACT,TOTSECID,TOTEMAIL,TOTNTUSR,ARR
+ S (TOTCNT,TOTNACT,TOTACT,TOTSECID,TOTEMAIL,TOTNTUSR,TOTVIS)=0
+ S U="^"
+ S XUDUZ=.9 F  S XUDUZ=$O(^VA(200,XUDUZ)) Q:+XUDUZ=0  D
+ .K ARR
+ .S TOTCNT=TOTCNT+1
+ .D GET(XUDUZ,.ARR)
+ .I $G(ARR("VISITOR"))=1 S TOTVIS=TOTVIS+1 Q  ;Filter Visitor records
+ .;Inactive New Person
+ .I SEL=3 D  Q
+ ..S NONACT=$$NONACT(.ARR)
+ ..I NONACT S TOTNACT=TOTNACT+1
+ .;Active New Person
+ .I SEL=2 D  Q
+ ..S ACT=$$ACT(.ARR) I 'ACT Q
+ ..S TOTACT=TOTACT+1
+ ..D GETACT(ACTSEL,.ARR)
+ .;All Active and Inactive New Person
+ .I SEL=1 D
+ ..S NONACT=$$NONACT(.ARR) I NONACT S TOTNACT=TOTNACT+1 Q
+ ..S TOTACT=TOTACT+1
+ ..D GETACT(ACTSEL,.ARR)
+ ;
+ ;RETURN - Total Count^Total Non Active^Total Active^Total SECID^Total Email
+ ;         ^Total NT User^Total Visitors
+ S RETURN=TOTCNT_U_TOTNACT_U_TOTACT_U_TOTSECID_U_TOTEMAIL_U_TOTNTUSR_U_TOTVIS
+ Q
+GET(XUDUZ,ARR) ;
+ ;Get all necessary fields from New Person file (#200)
+ N NPDATA
+ S DR="201;205.1;205.5;501.1;7;9.2",DIC=200,DA=XUDUZ,DIQ="NPDATA",DIQ(0)="I" D EN^DIQ1
+ S ARR("PRIMOPT")=$G(NPDATA(200,XUDUZ,201,"I"))
+ S ARR("SECID")=$G(NPDATA(200,XUDUZ,205.1,"I"))
+ S ARR("EMAIL")=$G(NPDATA(200,XUDUZ,205.5,"I"))
+ S ARR("NTUSR")=$G(NPDATA(200,XUDUZ,501.1,"I"))
+ S ARR("DISUSR")=$G(NPDATA(200,XUDUZ,7,"I"))
+ S ARR("TERMDT")=$G(NPDATA(200,XUDUZ,9.2,"I"))
+ I $O(^VA(200,XUDUZ,8910,0)) S ARR("VSTDATA")=1
+ I $G(ARR("VSTDATA")),$G(ARR("PRIMOPT"))'="" S ARR("VISITOR")=1
+ K DIC,DA,DR,DIQ
+ Q
+NONACT(ARR) ;
+ ;Inactive Person - Disuser=Y and/or Termination date is not a future date
+ I $G(ARR("DISUSR"))=1!(($G(ARR("TERMDT"))'="")&($G(ARR("TERMDT"))'>DT)) Q 1
+ Q 0
+ACT(ARR) ;
+ ;Active Person - not Disuser=Y and/or no Termination date.
+ I $G(ARR("DISUSR"))'=1,($G(ARR("TERMDT"))=""!($G(ARR("TERMDT"))>DT)) Q 1
+ Q 0
+GETACT(ACTSEL,ARR) ;
+ ;Aggregate total Active records has a SECID value
+ I ACTSEL=2,$G(ARR("SECID"))'="" S TOTSECID=TOTSECID+1 Q
+ ;Agreegate total Active records has an Email value
+ I ACTSEL=3,$G(ARR("EMAIL"))'="" S TOTEMAIL=TOTEMAIL+1 Q
+ ;Aggregate total Active records has a NT UserName value
+ I ACTSEL=4,$G(ARR("NTUSR"))'="" S TOTNTUSR=TOTNTUSR+1 Q
+ ;Aggregate all - SECID, AUPDN and NT UserName
+ I ACTSEL=1 D
+ .I $G(ARR("SECID"))'="" S TOTSECID=TOTSECID+1
+ .I $G(ARR("EMAIL"))'="" S TOTEMAIL=TOTEMAIL+1
+ .I $G(ARR("NTUSR"))'="" S TOTNTUSR=TOTNTUSR+1
+ Q
