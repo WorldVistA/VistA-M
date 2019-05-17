7ddda722930d4f22eb7f038adb194fb845240a96
Joseph Snyder <joe.snyder@kitware.com>
2019-05-17 17:04:07 -0400

    Add PPS-N updates from February to May 2019
    he following PPS-N/NDF Update file(s) are available for install:
    
          1)     PPS_26PRV_27NEW.DAT
          2)     PPS_27PRV_28NEW.DAT
          3)     PPS_28PRV_29NEW.DAT
          4)     PPS_29PRV_30NEW.DAT
          5)     PPS_30PRV_31NEW.DAT
          6)     PPS_31PRV_32NEW.DAT
    
    The patchs were aquired from
    https://foia-vista.osehra.org/Patches_By_Application/PSN-NATIONAL%20DRUG%20FILE%20(NDF)/PPS_DATS/
    on May 17, 2019

--- Packages/Master Patient Index VistA/Routines/MPIPSIM.VistAWebServicePort.1.m
+++
@@ -1,6 +0,0 @@
- ;MPIPSIM.VistAWebServicePort.1
- ;(C)InterSystems, generated for class MPIPSIM.VistAWebServicePort.  Do NOT edit. 03/14/2019 02:01:35PM
- ;;692F4645;MPIPSIM.VistAWebServicePort
- ;
-zexecute(requestXML) public {
- Quit ..WebMethod("execute").Invoke($this,"",.requestXML) }
