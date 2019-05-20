a2078de8b54286300b81fba4c8a341c9fcf50937
Joseph Snyder <joe.snyder@kitware.com>
2019-05-20 17:42:50 -0400

    Install: SD*5.3*721
    Patch Subject:  EDIT COMPUTER GENERATED APPT OPTION UPDATE
    Description:
    
    ============
     This patch addresses the following issue(s):
     1. An undefined error occurs when running the Edit Computer
     Generated Appointment Type [SDACS CGEDIT] option. This error
     occurs because the OUTPATIENT ENCOUNTER (#409.68) file entry
     is missing the PATIENT (#.02) field.
     2. The Edit Computer Generated Appointment Type [SDACS CGEDIT]
     option does not contain the necessary patient information for
     the affected end user (AEU) to determine the appointment type
     for computer generated appointments.
     Patch Components:
     -----------------
     Files & Fields Associated:
     File Name (Number)          Field Name (Number)       New/Modified/Deleted
     ------------------          -------------------       --------------------
     N/A
     Forms Associated:
     Form Name                   File Number               New/Modified/Deleted
     ---------                   -----------               --------------------
     N/A
     Mail Groups Associated:
     Mail Group Name             New/Modified/Deleted
     ---------------             --------------------
     N/A
     Options Associated:
     Option Name                 Type                      New/Modified/Deleted
     -----------                 ----                      --------------------
     N/A
     Protocols Associated:
     Protocol Name               New/Modified/Deleted
     -------------               --------------------
     N/A
     Security Keys Associated:
     Security Key Name
     -----------------
     N/A
     Templates Associated:
     Template Name         Type    File Name (Number)      New/Modified/Deleted
     -------------         ----    ------------------      --------------------
     N/A
     Remote Procedures Associated:
     Remote Procedure Name       New/Modified/Deleted
     ---------------------       --------------------
     N/A
     Parameter Definitions Associated:
     Parameter Name              New/Modified/Deleted
     --------------              --------------------
     N/A
     Additional Information:
     -----------------------
     Blood Bank Team Coordination:  N/A
     New Service Requests (NSRs):  N/A
     Patient Safety Issues (PSIs):  N/A
     Defect Tracking System Ticket(s) & Overview:
     1.  INC3582232 - <UNDEFINED>DEMW+1^SDACSCG *VA("PID") at NYH
     Problem:
     --------
     An undefined error occurs when running the Edit Computer Generated
     Appointment Type [SDACS CGEDIT] option. This error occurs because
     the OUTPATIENT ENCOUNTER (#409.68) file entry is missing the
     PATIENT (#.02) field.
     Resolution:
     -----------
     Routine SDACSCG was modified to ignore records that are missing the
     PATIENT (#.02) field. These are not valid records since the PATIENT
     (#.02) field is required for the OUTPATIENT ENCOUNTER (#409.68) file.
     2.  INC3173455 - Issue with Computer Generated Error Report
     Problem:
     --------
     The Edit Computer Generated Appointment Type [SDACS CGEDIT] option
     does not contain the necessary patient information for the affected
     end user (AEU) to determine the appointment type for computer generated
     appointments.
     Resolution:
     -----------
     Routine SDACSCG was modified to add the information requested by the
     AEU(s) needed to determine the appointment type for computer generated
     appointments. These entry fields include the division, clinic location,
     stop code, service connected, rated disability information, primary
     eligibility, secondary eligibility, and current eligibility for the
     encounter.
     Test Sites:
     -----------
     Coatesville VAMC
     VA New York Harbor HCS
     Software and Documentation Retrieval Instructions:
     --------------------------------------------------
     N/A
     Patch Installation:
     -------------------
     Pre/Post Installation Overview:
    
    Use default answers for KIDS load/install questions.
    
    Patch-Files: http://code.osehra.org/VistA.git/tree/master/Packages/Scheduling/Patches/SD_5.3_721

--- Packages/Scheduling/Routines/SDACSCG.m
+++ Packages/Scheduling/Routines/SDACSCG.m
@@ -1,5 +1,5 @@
 SDACSCG ;ALB/TET - Print/Edit Computer Generated Appt Types ;3/18/92  14:18
- ;;5.3;Scheduling;**16,22,132,202**;Aug 13, 1993
+ ;;5.3;Scheduling;**16,22,132,202,721**;Aug 13, 1993;Build 11
  ;
  Q
 CK ; -- check the number of computer generated visits
@@ -34,7 +34,7 @@ QUE ; -- queue entry point
  . . D DEM^VADPT
  . . D:$Y+6>IOSL CR,HDR
  . . Q:$D(DTOUT)!($D(DUOUT))
- . . W !,SDY,?25,$S(VAERR=0:VADM(1),1:"UNKNOWN"),?60,VA("PID")
+ . . W !,SDY,?25,$S(VAERR=0:VADM(1),1:"UNKNOWN"),?60,$G(VA("PID")) ;SD*5.3*721 - Protect VA("PID") variable
  . . S CT=CT+1
  I CT D:$Y+4>IOSL CR W !!,CT," MATCHES FOUND.",!
  ;
@@ -135,7 +135,7 @@ DPT ; -- look up in patient file & loop through acg for selected dfn
 DPTQ Q
  ;
 LOOP(SDBEG,SDEND,SDFN) ;
- N SDY,DFN,VA,VAERR,VAADM,SDT,SDOE
+ N SDY,DFN,VA,VAERR,VAADM,SDT,SDOE,SDCLIN,SDSTOP,SDDIV,SDSTA,SDPAT,SDDT,SDCELIG
  ;
  IF '$G(SDBEG) N SDBEG S SDBEG=0
  IF '$G(SDEND) N SDEND S SDEND=9999999
@@ -146,6 +146,13 @@ LOOP(SDBEG,SDEND,SDFN) ;
  . S SDOE=0
  . F  S SDOE=$O(^SCE("ACG",SDT,SDOE)) Q:'SDOE  D  Q:SDOUT 
  . . IF SDFN,SDFN'=+$P($G(^SCE(SDOE,0)),"^",2) Q
+ . . S SDPAT=+$P($G(^SCE(SDOE,0)),"^",2) Q:'SDPAT  ;SD*721 - Quit if no patient info in encounter
+ . . S SDDT=$P(+$P($G(^SCE(SDOE,0)),U),".") Q:'SDDT  ;SD*721 - Quit if not valid date
+ . . Q:$G(^DPT(SDPAT,0))=""  ;SD*721 - Quit if not in Patient File, supported by ICR #10035
+ . . S SDCELIG=$$GET1^DIQ(409.68,SDOE,.13)  ;SD*721 - Get Current Eligibility
+ . . S SDCLIN=$$GET1^DIQ(409.68,SDOE,.04),SDSTOP=$$GET1^DIQ(409.68,SDOE,.03)
+ . . ;Reference to SITE^VASITE supported by ICR #10112
+ . . S SDDIV=$$GET1^DIQ(409.68,SDOE,.11,"I"),SDSTA=$P($$SITE^VASITE(SDDT,SDDIV),U,3)
  . . D DEM(SDOE),DEMW
  . . D DIE(SDOE)
 LOOPQ Q
@@ -159,7 +166,15 @@ DEM(SDOE) ; -- get pt name,ssn and visit date
  Q
  ;
 DEMW ; -- write patient demographics
- W !!,SDY,?25,$S(VAERR=0:VADM(1),1:"UNKNOWN"),?60,VA("PID")
+ ;SD*721 - Add header, division, location, stop code, and service connected information
+ N SDHASH
+ W @IOF,"ENCOUNTER DATE",?25,"PATIENT NAME",?60,"PATIENT SSN" ;Header - SD*721
+ W !,?3,"DIVISION",?15,"CLINIC LOCATION",?50,"STOP CODE" ;Header - SD*721
+ W ! F SDHASH=1:1:80 W "-" ;Header - SD*721
+ W !,SDY,?25,$S(VAERR=0:VADM(1),1:"UNKNOWN"),?60,$G(VA("PID")) ;SD*5.3*721 - Protect VA("PID") variable
+ W !,?3,SDSTA,?15,$S(SDCLIN'="":SDCLIN,1:"UNKNOWN LOCATION"),?50,SDSTOP ;SD*721 - division, location, stop code
+ D SC(SDPAT) ;SD*721 - Service connected info
+ K VAEL,DFN
  Q
  ;
 DIE(SDOE) ; -- do edit
@@ -172,3 +187,26 @@ DIE(SDOE) ; -- do edit
  S CT=CT+1
  Q
  ;
+SC(DFN) ;SD*721 - Add Service Connected Information
+ ; Input  -- DFN      Patient file IEN
+ N SDI,SDCNT,SDDC,SDRD0,SDARR,SDSECN
+ ;Reference to ELIG^VADPT supported by ICR #10061
+ D ELIG^VADPT
+ W !!,"Patient's Service Connection and Rated Disabilities:"
+ W !!,$S(+VAEL(3)="1":"        SC Percent: "_$P(VAEL(3),U,2)_"%",1:" Service Connected: No")
+ W !,"Rated Disabilities: "
+ I 'VAEL(4) W "Not a Veteran",!,"Primary Eligibility Code: ",$S(+VAEL(1):$P(VAEL(1),U,2),1:"N/A") D  W !!,"Current Eligibility for Encounter: ",$S(SDCELIG'="":SDCELIG,1:"N/A") Q
+ .S SDSECN=0 F  S SDSECN=$O(VAEL(1,SDSECN)) Q:'SDSECN  W !,"Secondary Eligibility Code: ",$P(VAEL(1,SDSECN),U,2)
+ S (SDCNT,SDI)=0
+ ;Reference to ^DPT(DFN,.372 supported by ICR #1476
+ F  S SDI=$O(^DPT(DFN,.372,SDI)) Q:'SDI  I $P($G(^(SDI,0)),"^",3) S SDRD0=^(0) D
+ .S SDCNT=SDCNT+1
+ .;Reference to ^DIC(31,+SDRD0 supported by ICR #733
+ .S SDDC=$P($G(^DIC(31,+SDRD0,0)),U)
+ .W:SDCNT>1 !
+ .W ?20,SDDC,"  (",$P(SDRD0,"^",2),"%-",$S($P(SDRD0,"^",3):"SC",1:""),")"
+ I 'SDCNT W $S('$O(^DPT(DFN,.372,0)):"None Stated",1:"No Service Connected Disabilities Listed")
+ W !,"Primary Eligibility Code: ",$S(+VAEL(1):$P(VAEL(1),U,2),1:"N/A") D
+ .S SDSECN=0 F  S SDSECN=$O(VAEL(1,SDSECN)) Q:'SDSECN  W !,"Secondary Eligibility Code: ",$P(VAEL(1,SDSECN),U,2)
+ W !!,"Current Eligibility for Encounter: ",$S(SDCELIG'="":SDCELIG,1:"N/A")
+ Q
